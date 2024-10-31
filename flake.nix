328f45bda79fdb01b4828754e9f521002edeafab
Joey Sonnier <tijso@protonmail.com>
2024-10-31 06:04:28 -0500

    updated

--- flake.nix
+++ flake.nix
@@ -83,6 +83,7 @@
             ./home/home.nix
             nixvim.homeManagerModules.nixvim
             catppuccin.homeManagerModules.catppuccin
+            nix-colors
           ];
         };
       };
