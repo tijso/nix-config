374e8b5b61236cb3acdfb32c4f21c5cd6512ac15
Joey Sonnier <tijso@protonmail.com>
2024-11-01 02:04:02 -0500

    updated system

--- home/desktop/hyprland/default.nix
+++ home/desktop/hyprland/default.nix
@@ -26,23 +26,6 @@
     wayland-utils
     wayland-protocols
     ydotool
-    # sugar.sddm-sugar-dark # Name: sugar-dark
-    # tokyo-night # Name: tokyo-night-sddm
-    # libsForQt5.qt5.qtgraphicaleffects
   ];
 
-  # programs.hyprland = {
-  #   enable = true;
-  #   xwayland.enable = true;
-  #   package = inputs.hyprland.packages."${pkgs.system}".hyprland;
-  #   portalPackage = inputs.hyprland.packages.${pkgs.system}.xdg-desktop-portal-hyprland;
-  # };
-
-  # wayland.windowManager.hyprland = {
-  #   enable = true;
-  #   xwayland.enable = true;
-  #   package = inputs.hyprland.packages."${pkgs.system}".hyprland;
-  #   # plugins = [ inputs.hyprspace.packages.${pkgs.system}.Hyprspace ];
-  #   # catppuccin.enable = true;
-  # };
 }
