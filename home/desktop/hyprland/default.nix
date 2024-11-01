6cb8876a21074cb27352588b290fd7b41d230700
Joey Sonnier <tijso@protonmail.com>
2024-11-01 02:56:19 -0500

    updated system

--- home/desktop/hyprland/default.nix
+++ home/desktop/hyprland/default.nix
@@ -6,9 +6,8 @@
   ...
 }: {
   imports = [
-    ./binds.nix
+    ./bindings.nix
     ./rules.nix
-    ./general.nix
     ./theme.nix
     ./animations.nix
   ];
@@ -28,4 +27,85 @@
     ydotool
   ];
 
+  wayland.windowManager.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+    systemd.enable = true;
+    package = inputs.hyprland.packages."${pkgs.system}".hyprland;
+
+    settings = {
+      "$mod" = "SUPER";
+
+      exec-once = [
+        "dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
+        "systemctl --user import-environment QT_QPA_PLATFORMTHEME WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
+        "waybar"
+        "swaync"
+        "swww init"
+        # "nm-applet --indicator"
+
+        # "exec-once = swww img /home/${username}/Pictures/Wallpapers/1331008.png"
+
+        # "[workspace 1 silent] discord"
+        # "[workspace 6 silent] brave"
+        # "[workspace 6 silent] wezterm start btop"
+        # "[workspace 10 silent] qbittorrent"
+      ];
+
+      monitor = [
+        "DP-1,2560x1440,1920x0, 1"
+        "DP-2,1920x108, 0x0 1"
+      ];
+
+      env = [
+        "NIXOS_OZONE_WL,1"
+        "XDG_CURRENT_DESKTOP,Hyprland"
+        "XDG_SESSION_DESKTOP,Hyprland"
+        "GDK_BACKEND, wayland, x11"
+        "CLUTTER_BACKEND, wayland"
+        "XDG_SESSION_TYPE,wayland"
+        "QT_AUTO_SCREEN_SCALE_FACTOR,1"
+        "QT_QPA_PLATFORM=wayland,xcb"
+        "QT_WAYLAND_DISABLE_WINDOWDECORATION,1"
+        "MOZ_ENABLE_WAYLAND,1"
+        "SDL_VIDEODRIVER,wayland"
+      ];
+
+      input = {
+        kb_layout = "us";
+        follow_mouse = 1;
+        touchpad = {
+          natural_scroll = false;
+        };
+
+        sensitivity = -0.5;
+        repeat_delay = 200;
+        repeat_rate = 50;
+      };
+
+      misc = {
+        key_press_enables_dpms = true;
+        disable_hyprland_logo = true;
+        disable_splash_rendering = true;
+        background_color = "rgb(${config.colorScheme.palette.base01})";
+      };
+
+      dwindle = {
+        pseudotile = true;
+        preserve_split = true;
+        no_gaps_when_only = 0;
+        force_split = 2;
+      };
+
+      master = {
+        new_is_master = true;
+        no_gaps_when_only = 0;
+        mfact = 0.3;
+      };
+
+      gestures = {
+        workspace_swipe = false;
+      };
+    };
+  };
 }
