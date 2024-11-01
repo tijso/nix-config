28c3186ce6f127b96b9af098fc3cf72746838fa2
Joey Sonnier <tijso@protonmail.com>
2024-11-01 15:19:48 -0500

    updated system

--- home/local.nix
+++ home/local.nix
@@ -1,16 +1,49 @@
-{
-  pkgs,
-  ...
-}: {
+{pkgs, ...}: {
   imports = [
-    nix-colors.homeManagerModules.default
     ./cli
     ./terminals
     ./editors
     ./desktop
   ];
 
-  colorScheme = nix-colors.colorSchemes.catppuccin-mocha;
+  home.packages = with pkgs; [
+    tree
+    btop
+    logseq
+    inkscape
+    ookla-speedtest
+    gnome-disk-utility
+    obs-studio
+    baobab
+    cava
+    telegram-desktop
+    gimp
+    corefonts
+    cachix
+    qbittorrent
+    jamesdsp
+    megasync
+    vesktop
+    ani-cli
+  ];
 
-
-  }
+  programs.git = {
+    enable = true;
+    userName = "Joey Sonnier";
+    userEmail = "tijso@protonmail.com";
+    extraConfig = {
+      core.editor = "nvim";
+    };
+    aliases = {
+      a = "!git add . && git status";
+      d = "difftool";
+      lg = "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr)%C(bold blue)<%an>%Creset' --abbrev-commit";
+      pu = "push";
+      co = "checkout";
+      cm = "commit";
+    };
+    extraConfig = {
+      diff.tool = "vimdiff";
+    };
+  };
+}
