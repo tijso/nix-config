57501b27491288026e36d205f185b469b4eae028
Joey Sonnier <tijso@protonmail.com>
2024-11-01 13:35:55 -0500

    updated system

---
+++ users/global.nix
@@ -0,0 +1,26 @@
+{ pkgs, ... }: {
+  imports = [
+    ../../home/environments/hyprland/software/global.nix
+    ../../home/software/global.nix
+    ../../devices/printers/tsc-te244
+    ../../devices/printers
+
+    ../../common/audio
+    ../../common/boot.nix
+    ../../common/filesystem.nix
+    ../../common/gdm.nix
+    ../../common/general.nix
+    ../../common/keyring.nix
+    ../../common/network.nix
+    ../../common/touchpad.nix
+  ];
+
+  # program.fish.enable = true;
+  users.users.tijso = {
+    home = "/home/tijso";
+    description = "Prayag Jain";
+    isNormalUser = true;
+    extraGroups = [ "wheel" "networkmanager" "storage" "libvirtd" ];
+    shell = pkgs.fish;
+  };
+}
