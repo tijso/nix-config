5ba56a82da9010de36d3e945290385f381c9dd92
Joey Sonnier <tijso@protonmail.com>
2024-10-31 19:27:25 -0500

    updated

--- home/cli/fish.nix
+++ home/cli/fish.nix
@@ -13,67 +13,55 @@
       #fastfetch
 
       # Catppuccin
-      set -g fish_color_normal cdd6f4
-      set -g fish_color_command 89b4fa
-      set -g fish_color_param f2cdcd
-      set -g fish_color_keyword f38ba8
-      set -g fish_color_quote a6e3a1
-      set -g fish_color_end fab387
-      set -g fish_color_comment 7f849c
-      set -g fish_color_error f38ba8
-      set -g fish_color_gray 6c7086
-      set -g fish_color_selection --background=313244
-      set -g fish_color_search_match --background=313244
-      set -g fish_color_option a6e3a1
-      set -g fish_color_operator f5c2e7
-      set -g fish_color_escape eba0ac
-      set -g fish_color_autosuggestion 6c7086
-      set -g fish_color_cancel f38ba8
-      set -g fish_color_cwd f9e2af
-      set -g fish_color_user 94e2d5
-      set -g fish_color_host 89b4fa
-      set -g fish_color_host_remote a6e3a1
-      set -g fish_color_status f38ba8
+      # set -g fish_color_normal cdd6f4
+      # set -g fish_color_command 89b4fa
+      # set -g fish_color_param f2cdcd
+      # set -g fish_color_keyword f38ba8
+      # set -g fish_color_quote a6e3a1
+      # set -g fish_color_end fab387
+      # set -g fish_color_comment 7f849c
+      # set -g fish_color_error f38ba8
+      # set -g fish_color_gray 6c7086
+      # set -g fish_color_selection --background=313244
+      # set -g fish_color_search_match --background=313244
+      # set -g fish_color_option a6e3a1
+      # set -g fish_color_operator f5c2e7
+      # set -g fish_color_escape eba0ac
+      # set -g fish_color_autosuggestion 6c7086
+      # set -g fish_color_cancel f38ba8
+      # set -g fish_color_cwd f9e2af
+      # set -g fish_color_user 94e2d5
+      # set -g fish_color_host 89b4fa
+      # set -g fish_color_host_remote a6e3a1
+      # set -g fish_color_status f38ba8
 
       # Completion Pager Colors
-      set -g fish_pager_color_progress    6c7086
-      set -g fish_pager_color_prefix      f5c2e7
-      set -g fish_pager_color_completion  cdd6f4
-      set -g fish_pager_color_description 6c7086
+      # set -g fish_pager_color_progress    6c7086
+      # set -g fish_pager_color_prefix      f5c2e7
+      # set -g fish_pager_color_completion  cdd6f4
+      # set -g fish_pager_color_description 6c7086
 
       # Kanagawa
-      # set -l foreground DCD7BA normal
-      # set -l selection 2D4F67 brcyan
-      # set -l comment 727169 brblack
-      # set -l red C34043 red
-      # set -l orange FF9E64 brred
-      # set -l yellow C0A36E yellow
-      # set -l green 76946A green
-      # set -l purple 957FB8 magenta
-      # set -l cyan 7AA89F cyan
-      # set -l pink D27E99 brmagenta
-
-      # Syntax Highlighting Colors
-      # set -g fish_color_normal $foreground
-      # set -g fish_color_command $cyan
-      # set -g fish_color_keyword $pink
-      # set -g fish_color_quote $yellow
-      # set -g fish_color_redirection $foreground
-      # set -g fish_color_end $orange
-      # set -g fish_color_error $red
-      # set -g fish_color_param $purple
-      # set -g fish_color_comment $comment
-      # set -g fish_color_selection --background=$selection
-      # set -g fish_color_search_match --background=$selection
-      # set -g fish_color_operator $green
-      # set -g fish_color_escape $pink
-      # set -g fish_color_autosuggestion $comment
+      set -g fish_color_normal DCD7BA
+      set -g fish_color_command 7AA89F
+      set -g fish_color_keyword D27E99
+      set -g fish_color_quote C0A36E
+      set -g fish_color_redirection DCD7BA
+      set -g fish_color_end FF9E64
+      set -g fish_color_error C34043
+      set -g fish_color_param 957FB8
+      set -g fish_color_comment 727169
+      set -g fish_color_selection --background=2D4F67
+      set -g fish_color_search_match --background=2D4F67
+      set -g fish_color_operator 76946A
+      set -g fish_color_escape D27E99
+      set -g fish_color_autosuggestion 727169
 
       # Completion Pager Colors
-      # set -g fish_pager_color_progress $comment
-      # set -g fish_pager_color_prefix $cyan
-      # set -g fish_pager_color_completion $foreground
-      # set -g fish_pager_color_description $comment
+      set -g fish_pager_color_progress 727169
+      set -g fish_pager_color_prefix 7AA89F
+      set -g fish_pager_color_completion DCD7BA
+      set -g fish_pager_color_description 727169
     '';
 
     shellAliases = {
