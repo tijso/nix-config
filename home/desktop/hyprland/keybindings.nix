aa521b9061699b0e9f8a42a106e4a4f818a21f0d
Joey Sonnier <tijso@protonmail.com>
2024-11-01 09:28:50 -0500

    updated system

--- home/desktop/hyprland/keybindings.nix
+++ home/desktop/hyprland/keybindings.nix
@@ -1,58 +1,57 @@
 {pkgs, ...}: {
   wayland.windowManager.hyprland.settings = {
     "$mod" = "SUPER";
-    bind =
-      [
-        # Programs
-        "$mod, Q, killactive,"
-        "$mod  SHIFT, C, exit,"
-        "$mod, RETURN, exec, wezterm"
-        "$mod, B, exec, brave"
-        "$mod, E, exec, thunar"
-        "$mod, D, exec, grim -g  discord"
+    bind = [
+      # Programs
+      "$mod, Q, killactive,"
+      "$mod  SHIFT, C, exit,"
+      "$mod, RETURN, exec, wezterm"
+      "$mod, B, exec, brave"
+      "$mod, E, exec, thunar"
+      "$mod, D, exec, grim -g  discord"
 
-        # Rofi
-        "$mod, R, exec, rofi -show drun -theme $rofi_theme $focus_rofi"
-        "$mod, Equal, exec, rofi -modi calc -show calc -theme $rofi_theme $focus_rofi"
-        "$mod, N, exec, dunstctl action $focus_rofi"
-        "$mod, V, exec, cliphist list | rofi -dmenu -p C -theme $rofi_theme | cliphist decode | wl-copy $focus_rofi"
+      # Rofi
+      "$mod, R, exec, rofi -show drun -theme $rofi_theme $focus_rofi"
+      "$mod, Equal, exec, rofi -modi calc -show calc -theme $rofi_theme $focus_rofi"
+      "$mod, N, exec, dunstctl action $focus_rofi"
+      "$mod, V, exec, cliphist list | rofi -dmenu -p C -theme $rofi_theme | cliphist decode | wl-copy $focus_rofi"
 
-        # Tiling
-        "$mod, F, fullscreen,"
-        "$mod, T, togglefloating,"
-        "$mod, H, movefocus, l"
-        "$mod, L, movefocus, r"
-        "$mod, K, movefocus, u"
-        "$mod, J, movefocus, d"
+      # Tiling
+      "$mod, F, fullscreen,"
+      "$mod, T, togglefloating,"
+      "$mod, H, movefocus, l"
+      "$mod, L, movefocus, r"
+      "$mod, K, movefocus, u"
+      "$mod, J, movefocus, d"
 
-        # Move window with mainMod + shift
-        "$mod SHIFT, H, movewindow, l"
-        "$mod SHIFT, L, movewindow, r"
-        "$mod SHIFT, K, movewindow, u"
-        "$mod SHIFT, J, movewindow, d"
+      # Move window with mainMod + shift
+      "$mod SHIFT, L, movewindow, r"
+      "$mod SHIFT, H, movewindow, l"
+      "$mod SHIFT, K, movewindow, u"
+      "$mod SHIFT, J, movewindow, d"
 
-        # Workspaces
-        # "$mod, bracketright, workspace, +1"
-        # "$mod, bracketleft, workspace, -1"
-        # "$mod, CONTROL, bracketright, movetoworkspace, +1"
-        # "$mod, CONTROL, bracketleft, movetoworkspace, -1"
+      # Workspaces
+      "$mod, CONTROL  L, workspace, +1"
+      "$mod, CONTROL  H, workspace, -1"
+      "$mod, CONTROL, K, movetoworkspace, +1"
+      "$mod, CONTROL, J, movetoworkspace, -1"
 
-        # Resizing
-        "$mod ALT, L, resizeactive, 20 0"
-        "$mod ALT, H, resizeactive, -20 0"
-        "$mod ALT, K, resizeactive, 0 -20"
-        "$mod ALT, J, resizeactive, 0 20"
+      # Resizing
+      "$mod ALT, L, resizeactive, 20 0"
+      "$mod ALT, H, resizeactive, -20 0"
+      "$mod ALT, K, resizeactive, 0 -20"
+      "$mod ALT, J, resizeactive, 0 20"
 
-        # Audio
-        "$mod, P, exec, playerctl play-pause"
-        "$mod, M, exec, swayosd-client --input-volume mute-toggle"
-        ", XF86AudioPlay, exec, playerctl play-pause"
-        ", XF86AudioNext, exec, playerctl next"
-        ", XF86AudioPrev, exec, playerctl prev"
-        ", XF86AudioMute, exec, swayosd-client --output-volume mute-toggle"
-        ", XF86AudioMicMute, exec, swayosd-client --input-volume mute-toggle"
-      ];
-      binde = [
+      # Audio
+      "$mod, P, exec, playerctl play-pause"
+      "$mod, M, exec, swayosd-client --input-volume mute-toggle"
+      ", XF86AudioPlay, exec, playerctl play-pause"
+      ", XF86AudioNext, exec, playerctl next"
+      ", XF86AudioPrev, exec, playerctl prev"
+      ", XF86AudioMute, exec, swayosd-client --output-volume mute-toggle"
+      ", XF86AudioMicMute, exec, swayosd-client --input-volume mute-toggle"
+    ];
+    binde = [
       ", XF86AudioRaiseVolume, exec, swayosd-client --output-volume raise"
       ", XF86AudioLowerVolume, exec, swayosd-client --output-volume lower"
       "SHIFT, XF86AudioRaiseVolume, exec, swayosd-client --input-volume raise"
@@ -64,9 +63,5 @@
       "$mod, mouse:272, movewindow"
       "$mod, mouse:273, resizewindow"
     ];
-
-    ++ map (i: workspace (toString i)) workspaceArr
-    ++ map (i: moveToWorkspace (toString i)) workspaceArr
-    ++ map (i: moveToWorkspaceSilent (toString i)) workspaceArr;
   };
 }
