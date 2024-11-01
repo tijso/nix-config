0b439eb14170eac1d34a46c391118b55f031cbef
Joey Sonnier <tijso@protonmail.com>
2024-11-01 09:53:43 -0500

    updated system

--- home/desktop/hyprland/keybindings.nix
+++ home/desktop/hyprland/keybindings.nix
@@ -54,6 +54,18 @@
       "$mod, 9, workspace, 9"
       "$mod, 0, workspace, 10"
 
+      # Move active window to a workspace with mainMod + SHIFT + [0-6]
+      "$mod SHIFT, 1, movetoworkspace, 1"
+      "$mod SHIFT, 2, movetoworkspace, 2"
+      "$mod SHIFT, 3, movetoworkspace, 3"
+      "$mod SHIFT, 4, movetoworkspace, 4"
+      "$mod SHIFT, 5, movetoworkspace, 5"
+      "$mod SHIFT, 6, movetoworkspace, 6"
+      "$mod SHIFT, 7, movetoworkspace, 7"
+      "$mod SHIFT, 8, movetoworkspace, 8"
+      "$mod SHIFT, 9, movetoworkspace, 9"
+      "$mod SHIFT, 0, movetoworkspace, 10"
+
       # Audio
       "$mod, P, exec, playerctl play-pause"
       "$mod, M, exec, swayosd-client --input-volume mute-toggle"
