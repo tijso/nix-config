9e996e9b8f0c4c6e159f7daef7b9899c6e74d4d8
Joey Sonnier <tijso@protonmail.com>
2024-11-02 14:32:14 -0500

    updated system

--- hosts/system/default.nix
+++ hosts/system/default.nix
@@ -1,12 +1,19 @@
 {...}: {
   imports = [
+    ./audio.nix
     ./bluetooth.nix
+    ./boot.nix
     # ./cosmic.nix
     ./disks.nix
+    ./gdm.nix
     ./gnome.nix
     ./graphics.nix
-    # ./hyprland.nix
+    ./hyprland.nix
+    ./keyring.nix
+    ./network.nix
     ./pkgs.nix
+    # ./sddm.nix
     # ./tlp.nix
+    ./touchpad.nix
   ];
 }
