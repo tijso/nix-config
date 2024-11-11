76cd114d736643caa3c8f580b5a4406cc6c46059
Joey Sonnier <tijso@protonmail.com>
2024-11-11 13:31:08 -0600

    updated gdm

--- environments/gdm.nix
+++ environments/gdm.nix
@@ -1,7 +1,12 @@
-{ ... }: {
+{...}: {
   services.xserver.displayManager.gdm = {
     enable = true;
     wayland = true;
+    theme = {
+      name = "Adwaita";
+      package = pkgs.gnome.adwaita-icon-theme;
+    };
   };
+
   security.pam.services.gdm.enableGnomeKeyring = true;
 }
