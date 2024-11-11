fc983085b1b27162b9009ce9ba411431966510cc
Joey Sonnier <tijso@protonmail.com>
2024-11-11 14:29:35 -0600

    updated gdm

--- environments/gdm.nix
+++ environments/gdm.nix
@@ -3,38 +3,6 @@
     enable = true;
     wayland = true;
     };
-  };
 
   security.pam.services.gdm.enableGnomeKeyring = true;
-}
-
-
-  { pkgs, ... }: {
-  services.xserver.displayManager.gdm = {
-    enable = true;
-    wayland = true;
-  };
-
-  # Enable required GNOME services
-  services.gnome.gnome-keyring.enable = true;
-  security.pam.services.gdm.enableGnomeKeyring = true;
-
-  # Enable dconf
-  programs.dconf.enable = true;
-
-  # Configure GDM theme through dconf
-  services.xserver.desktopManager.gnome.extraGSettingsOverrides = ''
-    [org.gnome.desktop.interface]
-    gtk-theme='Adwaita'
-    icon-theme='Adwaita'
-    cursor-theme='Adwaita'
-
-    [org.gnome.login-screen]
-    logo='';
-''
-  # Ensure the theme is available system-wide
-  environment.systemPackages = with pkgs; [
-    gnome.gnome-themes-extra
-    gnome.adwaita-icon-theme
-  ];
 }
