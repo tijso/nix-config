5971de931ad787802ca8f6dc8e7572f6fdfe24cf
Joey Sonnier <tijso@protonmail.com>
2024-11-11 00:36:45 -0600

    updated system

--- home/environments/gtk.nix
+++ home/environments/gtk.nix
@@ -7,7 +7,6 @@
   # Enable GTK configuration
   gtk = {
     enable = true;
-
     theme = {
       name = "Catppuccin-Mocha-Standard-Blue-Dark";
       package = pkgs.catppuccin-gtk.override {
@@ -18,34 +17,30 @@
       };
     };
 
-    # Ensure GTK applications prefer dark theme
-    gtk3.extraConfig = {
-      Settings = ''
-        gtk-application-prefer-dark-theme=1
-      '';
-    };
-
-    gtk4.extraConfig = {
-      Settings = ''
-        gtk-application-prefer-dark-theme=1
-      '';
-    };
-
-    # Optional: Configure icon theme to match
     iconTheme = {
       name = "Papirus-Dark";
       package = pkgs.papirus-icon-theme;
     };
 
-    # Optional: Configure cursor theme
     cursorTheme = {
-      name = "Catppuccin-Mocha-Dark-Cursors";
-      package = pkgs.catppuccin-cursors.mochaDark;
-      size = 24;
+      name = "Bibata-Modern-Ice";
+      package = pkgs.bibata-cursors;
+      size = 20;
     };
   };
 
-  # Optional: Configure qt5ct for Qt applications to match GTK theme
+  gtk3.extraConfig = {
+    Settings = ''
+      gtk-application-prefer-dark-theme=1
+    '';
+  };
+
+  gtk4.extraConfig = {
+    Settings = ''
+      gtk-application-prefer-dark-theme=1
+    '';
+  };
+
   qt = {
     enable = true;
     # platformTheme = "gtk";
