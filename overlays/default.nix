4a18d5c0509012e138c479dc329adf4bfee86b73
Joey Sonnier <tijso@protonmail.com>
2024-11-11 15:25:20 -0600

    updated config

--- overlays/default.nix
+++ overlays/default.nix
@@ -6,6 +6,16 @@
   # You can change versions, add patches, set compilation flags, anything really.
   # https://nixos.wiki/wiki/Overlays
   modifications = final: prev: {
+    catppuccin-gtk = prev.catppuccin-gtk.overrideAttrs (old: {
+        src = prev.fetchFromGitHub {
+          owner = "catppuccin";
+          repo = "gtk";
+          rev = "v${old.version}";
+          fetchSubmodules = true;
+          hash = "sha256-q5/VcFsm3vNEw55zq/vcM11eo456SYE5TQA3g2VQjGc=";
+        };
+        postUnpack = "";
+      });
   };
 
   # When applied, the unstable nixpkgs set (declared in the flake inputs) will
