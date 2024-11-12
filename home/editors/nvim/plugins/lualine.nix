5d9ee9486c54918135a49676c53a54310b7d3cd9
Joey Sonnier <tijso@protonmail.com>
2024-11-11 20:37:30 -0600

    updated nixvim

--- home/editors/nvim/plugins/lualine.nix
+++ home/editors/nvim/plugins/lualine.nix
@@ -2,107 +2,109 @@
   programs.nixvim = {
     plugins.lualine = {
       enable = true;
-      globalstatus = true;
-      theme = "auto";
-      componentSeparators = {
-        left = "|";
-        right = "|";
-      };
-      sectionSeparators = {
-        # left = "";
-        # right = "";
-        left = "";
-        right = "";
-      };
-      sections = {
-        lualine_a = [
-          {
-            name = "mode";
-            extraConfig = {
-              separator = {
-                # left = "";
-                # right = "";
-                # left = "";
-                # right = "";
+      settings = {
+        globalstatus = true;
+        theme = "auto";
+        componentSeparators = {
+          left = "|";
+          right = "|";
+        };
+        sectionSeparators = {
+          # left = "";
+          # right = "";
+          left = "";
+          right = "";
+        };
+        sections = {
+          lualine_a = [
+            {
+              name = "mode";
+              extraConfig = {
+                separator = {
+                  # left = "";
+                  # right = "";
+                  # left = "";
+                  # right = "";
+                };
               };
-            };
-          }
-        ];
-        lualine_b = [
-          {
-            name = "branch";
-            extraConfig = {
-              padding = {
-                left = 2;
-                right = 0;
+            }
+          ];
+          lualine_b = [
+            {
+              name = "branch";
+              extraConfig = {
+                padding = {
+                  left = 2;
+                  right = 0;
+                };
+                icon = "";
+                colored = true;
               };
-              icon = "";
-              colored = true;
-            };
-          }
-          {
-            name = "diff";
-            extraConfig = {
-              colored = true;
-              symbols = {
-                added = " ";
-                modified = " ";
-                removed = " ";
+            }
+            {
+              name = "diff";
+              extraConfig = {
+                colored = true;
+                symbols = {
+                  added = " ";
+                  modified = " ";
+                  removed = " ";
+                };
               };
-            };
-          }
-        ];
-        lualine_c = [
-          {
-            name = "filename";
-            extraConfig = {
-              colored = true;
-            };
-          }
-        ];
-        lualine_x = [
-          {
-            name = "diagnostics";
-            extraConfig = {
-              color = {
-                fg = "#605f6f";
-                bg = "#232232";
+            }
+          ];
+          lualine_c = [
+            {
+              name = "filename";
+              extraConfig = {
+                colored = true;
               };
-              diagnostics_color = {
-                color_error = {fg = "#F38BA8";};
-                color_warn = {fg = "#FAE3B0";};
+            }
+          ];
+          lualine_x = [
+            {
+              name = "diagnostics";
+              extraConfig = {
+                color = {
+                  fg = "#605f6f";
+                  bg = "#232232";
+                };
+                diagnostics_color = {
+                  color_error = {fg = "#F38BA8";};
+                  color_warn = {fg = "#FAE3B0";};
+                };
+                symbols = {
+                  error = " ";
+                  warn = " ";
+                };
               };
-              symbols = {
-                error = " ";
-                warn = " ";
+            }
+            {
+              name = "filetype";
+              extraConfig = {
+                colored = true;
+                padding = {
+                  left = 1;
+                  right = 2;
+                };
               };
-            };
-          }
-          {
-            name = "filetype";
-            extraConfig = {
-              colored = true;
-              padding = {
-                left = 1;
-                right = 2;
+            }
+          ];
+          lualine_y = ["progress"];
+          lualine_z = [
+            {
+              name = "location";
+              extraConfig = {
+                separator = {
+                  # right = "";
+                  # right = "";
+                  # left = "";
+                  # left = "";
+                };
               };
-            };
-          }
-        ];
-        lualine_y = ["progress"];
-        lualine_z = [
-          {
-            name = "location";
-            extraConfig = {
-              separator = {
-                # right = "";
-                # right = "";
-                # left = "";
-                # left = "";
-              };
-            };
-          }
-        ];
+            }
+          ];
+        };
       };
     };
   };
