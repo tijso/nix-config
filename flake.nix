f15e5376976fcea1c13dde1e30ebfcda71298901
Joey Sonnier <tijso@protonmail.com>
2024-11-13 12:40:05 -0600

    Updated System

--- flake.nix
+++ flake.nix
@@ -25,11 +25,7 @@
     # Programs
     nix-colors.url = "github:misterio77/nix-colors";
 
-    # nixvim = {
-    #   url = "github:nix-community/nixvim";
-    #   inputs.nixpkgs.follows = "nixpkgs-unstable";
-    # };
-
+    # Neovim Configuration
     nixvim.url = "github:tijso/nixvim";
 
     nixos-cosmic = {
@@ -39,62 +35,67 @@
 
     catppuccin.url = "github:catppuccin/nix";
   };
-  outputs = {
-    self,
-    nixpkgs,
-    systems,
-    home-manager,
-    nix-colors,
-    # nixvim,
-    nixos-cosmic,
-    catppuccin,
-    ...
-  } @ inputs: let
-    inherit (self) outputs;
-    lib = nixpkgs.lib // home-manager.lib;
-    forEachSystem = f: lib.genAttrs (import systems) (system: f pkgsFor.${system});
-    pkgsFor = lib.genAttrs (import systems) (
-      system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      systems,
+      home-manager,
+      nix-colors,
+      nixos-cosmic,
+      catppuccin,
+      ...
+    }@inputs:
+    let
+      inherit (self) outputs;
+      lib = nixpkgs.lib // home-manager.lib;
+      forEachSystem = f: lib.genAttrs (import systems) (system: f pkgsFor.${system});
+      pkgsFor = lib.genAttrs (import systems) (
+        system:
         import nixpkgs {
           inherit system;
           config.allowUnfree = true;
         }
-    );
-  in {
-    inherit lib;
-    overlays = import ./overlays {inherit inputs;};
+      );
+    in
+    {
+      inherit lib;
+      overlays = import ./overlays { inherit inputs; };
 
-    packages = forEachSystem (pkgs: import ./pkgs {inherit pkgs;});
-    devShells = forEachSystem (pkgs: import ./shell.nix {inherit pkgs;});
-    formatter = forEachSystem (pkgs: pkgs.alejandra);
+      packages = forEachSystem (pkgs: import ./pkgs { inherit pkgs; });
+      devShells = forEachSystem (pkgs: import ./shell.nix { inherit pkgs; });
+      formatter = forEachSystem (pkgs: pkgs.alejandra);
 
-    nixosConfigurations = {
-      serenity = lib.nixosSystem {
-        specialArgs = {inherit inputs outputs;};
-        modules = [
-          ./hosts/serenity
-          {
-            nix.settings = {
-              substituters = ["https://cosmic.cachix.org/"];
-              trusted-public-keys = ["cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="];
-            };
-          }
-          nixos-cosmic.nixosModules.default
-          catppuccin.nixosModules.catppuccin
-        ];
+      nixosConfigurations = {
+        serenity = lib.nixosSystem {
+          specialArgs = {
+            inherit inputs outputs;
+          };
+          modules = [
+            ./hosts/serenity
+            {
+              nix.settings = {
+                substituters = [ "https://cosmic.cachix.org/" ];
+                trusted-public-keys = [ "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE=" ];
+              };
+            }
+            nixos-cosmic.nixosModules.default
+            catppuccin.nixosModules.catppuccin
+          ];
+        };
+      };
+
+      homeConfigurations = {
+        "tijso@serenity" = lib.homeManagerConfiguration {
+          pkgs = pkgsFor.x86_64-linux;
+          extraSpecialArgs = {
+            inherit inputs outputs nix-colors;
+          };
+          modules = [
+            ./home/home.nix
+            catppuccin.homeManagerModules.catppuccin
+          ];
+        };
       };
     };
-
-    homeConfigurations = {
-      "tijso@serenity" = lib.homeManagerConfiguration {
-        pkgs = pkgsFor.x86_64-linux;
-        extraSpecialArgs = {inherit inputs outputs nix-colors;};
-        modules = [
-          ./home/home.nix
-          # nixvim.homeManagerModules.nixvim
-          catppuccin.homeManagerModules.catppuccin
-        ];
-      };
-    };
-  };
 }
