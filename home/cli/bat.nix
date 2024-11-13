d029cc92a7a4a8a1392b63678fef5501766af63d
Joey Sonnier <tijso@protonmail.com>
2024-11-13 16:56:39 -0600

    Updated Nixvim

--- home/cli/bat.nix
+++ home/cli/bat.nix
@@ -1,14 +1,15 @@
 {
   pkgs,
-  config,
   ...
-}: {
+}:
+{
   programs.bat = {
     enable = true;
     catppuccin.enable = true;
-    config = {
-      pager = "less -R";
-    };
+    style = "numbers,changes,header";
+    paging = "auto";
+    wrap = "never";
+    tabs = "4";
     extraPackages = with pkgs.bat-extras; [
       batdiff
       batgrep
