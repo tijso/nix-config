7ce9a65b15e71d96332b5e6db785703a308728dc
Joey Sonnier <tijso@protonmail.com>
2024-11-13 17:10:58 -0600

    Updated Flake

--- home/cli/bat.nix
+++ home/cli/bat.nix
@@ -5,8 +5,8 @@
 {
   programs.bat = {
     enable = true;
+    catppuccin.enable = true;
     config = {
-      catppuccin.enable = true;
       style = "numbers,changes,header";
       paging = "auto";
       wrap = "never";
