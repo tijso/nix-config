ee17af3918ee7fd271b411478cba970e78895c8a
Joey Sonnier <tijso@protonmail.com>
2024-11-14 16:06:58 -0600

    Changing Config Layout

--- development/git/default.nix
+++ development/git/default.nix
@@ -3,6 +3,7 @@
 {
   imports = [
     ./lazy_git.nix
+    ./github.nix
   ];
 
   programs.git = {
@@ -22,34 +23,4 @@
       pull.rebase = true;
     };
   };
-
-  environment.systemPackages = with pkgs; [
-    autoconf
-    coreutils-full
-    gcc
-    libgcc
-    meson
-    ncurses
-    ninja
-    openssl
-
-    elixir
-    go
-    python3
-    rustup
-
-    ccls
-    cmake-language-server
-    gopls
-    lua-language-server
-    nodePackages.bash-language-server
-    nodePackages.eslint
-    nodePackages.prettier
-    # nodePackages_latest.pyright
-    pyright
-    nodePackages.typescript
-    nodePackages.typescript-language-server
-    vscode-langservers-extracted
-    yaml-language-server
-  ];
 }
