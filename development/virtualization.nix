c06077067c1c7cb54b4b8d300c7136a5a22c08ad
Joey Sonnier <tijso@protonmail.com>
2024-11-14 21:35:31 -0600

    Changing Config Layout

--- development/virtualization.nix
+++ development/virtualization.nix
@@ -25,46 +25,5 @@ with lib;
       "kvm"
       "qemu"
     ];
-
-    # Configuration files
-    home.file = {
-      # QEMU config
-      ".config/qemu/qemu.conf".text = ''
-        # Enable shared memory
-        memory_backing_dir = "/dev/shm"
-
-        # Security settings
-        security_driver = "none"
-        remember_owner = false
-
-        # Network settings
-        vnc_listen = "0.0.0.0"
-        spice_listen = "0.0.0.0"
-      '';
-
-      # Default storage pool
-      ".config/libvirt/storage/default.xml".text = ''
-        <pool type="dir">
-          <name>default</name>
-          <target>
-            <path>${config.home.homeDirectory}/VirtualMachines</path>
-          </target>
-        </pool>
-      '';
-
-      # Network configuration
-      ".config/libvirt/qemu/networks/default.xml".text = ''
-        <network>
-          <name>default</name>
-          <bridge name="virbr0"/>
-          <forward mode="nat"/>
-          <ip address="192.168.122.1" netmask="255.255.255.0">
-            <dhcp>
-              <range start="192.168.122.2" end="192.168.122.254"/>
-            </dhcp>
-          </ip>
-        </network>
-      '';
-    };
   };
 }
