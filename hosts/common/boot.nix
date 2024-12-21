da1ab0fd2e68948a42f395833b452d29551fe4f1
Joey Sonnier <tijso@protonmail.com>
2024-12-20 19:07:36 -0600

    Changed plmouth theme to cybernetic

--- hosts/common/boot.nix
+++ hosts/common/boot.nix
@@ -6,7 +6,7 @@
     initrd.verbose = false;
     plymouth = {
       enable = true;
-      theme = "optimus";
+      theme = "cybernetic";
       themePackages = with pkgs; [
         (adi1090x-plymouth-themes.override {
           selected_themes = [
