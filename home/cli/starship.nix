7c4bb9aca663b11377c49f35df6ed57c74ab4f6d
Joey Sonnier <tijso@protonmail.com>
2024-12-22 12:53:29 -0600

    updated starship colors

--- home/cli/starship.nix
+++ home/cli/starship.nix
@@ -9,8 +9,8 @@
     enableFishIntegration = true;
     settings = {
       format = ''
-        [┌](fg:#${config.colorScheme.palette.base02}) $hostname$directory$nix_shell$golang$rust$python$git_branch$git_status$cmd_duration
-        [└](fg:#${config.colorScheme.palette.base02}) $os$character
+        [┌](fg:#${config.colorScheme.palette.base08}) $hostname$directory$nix_shell$golang$rust$python$git_branch$git_status$cmd_duration
+        [└](fg:#${config.colorScheme.palette.base08}) $os$character
       '';
 
       character = {
