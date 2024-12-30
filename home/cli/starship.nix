f19c987a4ceb843800a260aa62dd54e322c2f6b4
Joey Sonnier <tijso@protonmail.com>
2024-12-29 18:08:41 -0600

    updated prompted

--- home/cli/starship.nix
+++ home/cli/starship.nix
@@ -1,62 +1,95 @@
-{ pkgs, config, ... }:
-
+{ lib, inputs, ... }:
 {
   programs.starship = {
-    enable = true;
+    enable = false;
+
+    enableBashIntegration = true;
     enableZshIntegration = true;
-    enableFishIntegration = true;
+    enableNushellIntegration = true;
+
     settings = {
-      add_newline = true;
-      character = {
-        success_symbol = "[❯](#cba6f7)[❯](#89dceb)[❯](#f2cdcd)";
-        error_symbol = "[❯](#fab387)[❯](#eba0ac)[❯](#6c7086)";
+      format = lib.concatStrings [
+        "[](color_orange)"
+        "$os"
+        "[](bg:color_yellow fg:color_orange)"
+        "$directory"
+        "[](fg:color_yellow bg:color_aqua)"
+        "$git_branch"
+        "$git_status"
+        "[](fg:color_aqua bg:color_blue)"
+        "$nix_shell"
+        "[](fg:color_blue bg:color_bg3)"
+        "$cmd_duration"
+        "[](fg:color_bg3) "
+      ];
+
+      palette = "gruvbox_dark";
+      palettes.gruvbox_dark = {
+        color_fg0 = "#fbf1c7";
+        color_bg1 = "#3c3836";
+        color_bg3 = "#665c54";
+        color_blue = "#458588";
+        color_aqua = "#689d6a";
+        color_green = "#98971a";
+        color_orange = "#d65d0e";
+        color_purple = "#b16286";
+        color_red = "#cc241d";
+        color_yellow = "#d79921";
       };
-      username = {
-        show_always = true;
-        disabled = true;
-        format = "[$user@]($style)";
-        style_user = "#585b70";
+
+      os = {
+        disabled = false;
+        style = "bg:color_orange bold fg:color_fg0";
+        symbols = {
+          NixOS = " ";
+        };
       };
-      hostname = {
-        ssh_only = false;
-        format = "[$hostname]($style) ";
-        style = "#f5c2e7";
-      };
+
       directory = {
-        truncation_length = 1;
-        truncation_symbol = "";
-        fish_style_pwd_dir_length = 1;
+        style = "bold fg:color_fg0 bg:color_yellow";
+        format = "[ $path ]($style)";
+        truncation_length = 3;
       };
+
       git_branch = {
-        style = "#cba6f7";
+        symbol = "";
+        style = "bg:color_aqua";
+        format = "[[ $symbol $branch ](bold fg:color_fg0 bg:color_aqua)]($style)";
       };
+
       git_status = {
-        conflicted = " ";
-        ahead = " ";
-        behind = " 󱊾 ";
-        diverged = "󱡷 ";
-        untracked = "";
-        stashed = " ";
-        modified = " ";
-        staged = " ";
-        renamed = " ";
-        deleted = "󰆴 ";
-        style = "#7f849c";
+        style = "bg:color_aqua bold fg:color_fg0";
+        format = "[$all_status$ahead_behind]($style)";
       };
-      golang = {
-        symbol = "ﳑ ";
-        style = "#212736";
-        format = "[[ $symbol ($version) ](fg:#89b4fa bg:#6c7086)]($style)";
+
+      nix_shell = {
+        format = "[ via nix $name ]($style)";
+        style = "bg:color_blue bold fg:color_fg0";
       };
-      rust = {
-        format = "[$symbol$version]($style)";
-        symbol = " ";
-        style = "#94e2d5";
+
+      time = {
+        disabled = false;
+        time_format = "%R";
+        style = "bg:color_bg1";
+        format = "[[   $time ](fg:color_fg0 bg:color_bg1)]($style)";
       };
-      python = {
-        format = "[$symbol$version]($style)";
-        symbol = "󰌠 ";
-        style = "#94e2d5";
+
+      cmd_duration = {
+        format = "[ 󰔛 $duration ]($style)";
+        disabled = false;
+        style = "bg:color_bg3 fg:color_fg0";
+        show_notifications = false;
+        min_time_to_notify = 60000;
+      };
+
+      line_break = {
+        disabled = false;
+      };
+
+      character = {
+        disabled = false;
+        success_symbol = "[  ](bold fg:color_green)";
+        error_symbol = "[  ](bold fg:color_red)";
       };
     };
   };
