dd1070bc3920ad6d7d8a0befab0042ea5cab37ac
Joey Sonnier <tijso@protonmail.com>
2024-12-30 16:16:52 -0600

    added theme to wez term

--- home/terminals/wezterm.nix
+++ home/terminals/wezterm.nix
@@ -9,10 +9,22 @@
     extraConfig = ''
       return {
         -- color_scheme = "Catppuccin Mocha",
-        color_scheme = "Tokyo Night",
+        -- color_scheme = "Tokyo Night",
         -- color_scheme = "rose-pine",
         -- color_scheme = "Kanagawa (Gogh)",
         -- font = wezterm.font "Fira Code",
+        -- my coolnight colorscheme:
+        config.colors = {
+        	foreground = "#CBE0F0",
+        	background = "#011423",
+        	cursor_bg = "#47FF9C",
+        	cursor_border = "#47FF9C",
+        	cursor_fg = "#011423",
+        	selection_bg = "#033259",
+        	selection_fg = "#CBE0F0",
+        	ansi = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#0FC5ED", "#a277ff", "#24EAF7", "#24EAF7" },
+        	brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
+        },
         font = wezterm.font "Maple Mono",
         -- font = wezterm.font "Monaspace Radon",
         --font_rules = {
