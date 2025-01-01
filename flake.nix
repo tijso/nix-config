3e8a3ca7b0a7687faaef5be2c89ed7a42ae60b93
Joey Sonnier <tijso@protonmail.com>
2025-01-01 06:04:58 -0600

    Updated Flake

--- flake.nix
+++ flake.nix
@@ -41,75 +41,71 @@
     ghostty = {
       url = "github:ghostty-org/ghostty";
     };
-
   };
 
-  outputs =
-    {
-      self,
-      nixpkgs,
-      home-manager,
-      systems,
-      nix-colors,
-      nixos-cosmic,
-      nixvim,
-      catppuccin,
-      hyprland,
-      grub2-themes,
-      ghostty,
-      ...
-    }@inputs:
-    let
-      inherit (self) outputs;
-      lib = nixpkgs.lib // home-manager.lib;
-      forEachSystem = f: lib.genAttrs (import systems) (system: f pkgsFor.${system});
-      pkgsFor = lib.genAttrs (import systems) (
-        system:
+  outputs = {
+    self,
+    nixpkgs,
+    home-manager,
+    systems,
+    nix-colors,
+    nixos-cosmic,
+    nixvim,
+    catppuccin,
+    hyprland,
+    grub2-themes,
+    ghostty,
+    ...
+  } @ inputs: let
+    inherit (self) outputs;
+    lib = nixpkgs.lib // home-manager.lib;
+    forEachSystem = f: lib.genAttrs (import systems) (system: f pkgsFor.${system});
+    pkgsFor = lib.genAttrs (import systems) (
+      system:
         import nixpkgs {
           inherit system;
           config.allowUnfree = true;
         }
-      );
-    in
-    {
-      inherit lib;
-      overlays = import ./overlays { inherit inputs; };
+    );
+  in {
+    inherit lib;
+    overlays = import ./overlays {inherit inputs;};
 
-      packages = forEachSystem (pkgs: import ./pkgs { inherit pkgs; });
-      devShells = forEachSystem (pkgs: import ./shell.nix { inherit pkgs; });
-      formatter = forEachSystem (pkgs: pkgs.alejandra);
+    packages = forEachSystem (pkgs: import ./pkgs {inherit pkgs;});
+    devShells = forEachSystem (pkgs: import ./shell.nix {inherit pkgs;});
+    formatter = forEachSystem (pkgs: pkgs.alejandra);
 
-      nixosConfigurations = {
-        serenity = lib.nixosSystem {
-          specialArgs = {
-            inherit inputs outputs;
-          };
-          modules = [
-            ./hosts/serenity
-            {
-              nix.settings = {
-                substituters = [ "https://cosmic.cachix.org/" ];
-                trusted-public-keys = [ "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE=" ];
-              };
-            }
-            nixos-cosmic.nixosModules.default
-            catppuccin.nixosModules.catppuccin
-            grub2-themes.nixosModules.default
-          ];
+    nixosConfigurations = {
+      serenity = lib.nixosSystem {
+        specialArgs = {
+          inherit inputs outputs;
         };
-      };
-
-      homeConfigurations = {
-        "tijso@serenity" = lib.homeManagerConfiguration {
-          pkgs = pkgsFor.x86_64-linux;
-          extraSpecialArgs = {
-            inherit inputs outputs;
-          };
-          modules = [
-            ./home/home.nix
-            catppuccin.homeManagerModules.catppuccin
-          ];
-        };
+        modules = [
+          ./hosts/serenity
+          {
+            nix.settings = {
+              substituters = ["https://cosmic.cachix.org/"];
+              trusted-public-keys = ["cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="];
+            };
+          }
+          nixos-cosmic.nixosModules.default
+          catppuccin.nixosModules.catppuccin
+          grub2-themes.nixosModules.default
+        ];
       };
     };
+
+    homeConfigurations = {
+      "tijso@serenity" = lib.homeManagerConfiguration {
+        pkgs = pkgsFor.x86_64-linux;
+        extraSpecialArgs = {
+          inherit inputs outputs;
+        };
+        modules = [
+          ./home/home.nix
+          catppuccin.homeManagerModules.catppuccin
+        ];
+      };
+    };
+  };
 }
