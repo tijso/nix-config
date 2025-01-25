d573f179056c112d2262892aed8816bbe583b23b
Joey Sonnier <tijso@protonmail.com>
2025-01-25 11:14:35 -0600

    Updated graphics

--- hosts/common/graphics.nix
+++ hosts/common/graphics.nix
@@ -1,20 +1,19 @@
 {
   pkgs,
-  lib,
-  inputs,
   ...
-}: {
+}:
+{
   hardware.graphics = {
     enable = true;
     extraPackages = with pkgs; [
       vulkan-loader
       vulkan-validation-layers
       vulkan-extension-layer
-      rocmPackages.clr.icd
+      # rocmPackages.clr.icd
       # amdvlk
     ];
   };
 
-  services.xserver.videoDrivers = ["amdgpu"];
-  boot.initrd.kernelModules = ["amdgpu"];
+  services.xserver.videoDrivers = [ "amdgpu" ];
+  boot.initrd.kernelModules = [ "amdgpu" ];
 }
