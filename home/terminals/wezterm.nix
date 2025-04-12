7f4589d5100cb5f16c10bb0f0f72cc466e321b2c
Joey Sonnier <tijso@protonmail.com>
2025-04-12 12:33:59 -0500

    changed the kitty font name

--- home/terminals/wezterm.nix
+++ home/terminals/wezterm.nix
@@ -33,7 +33,7 @@ in
         	      brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
                 },
 
-                font = wezterm.font "Maple Mono",
+                font = wezterm.font "Maple Mono NF",
                 -- font = wezterm.font "Monaspace Radon",
                 --font_rules = {
                 --{
