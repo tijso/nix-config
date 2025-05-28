46999ca517aadcaa3377e030c5f5d815dbd9d30b
Joey Sonnier <tijso@protonmail.com>
2025-05-28 13:10:25 -0500

    changed theme

--- home/theming/gtk.nix
+++ home/theming/gtk.nix
@@ -14,64 +14,65 @@ in
     gtk = {
       enable = true;
       theme = {
-        name = "Tokyonight-Dark-BL";
-        package = pkgs.tokyonight-gtk-theme.override {
-          themeVariants = [ "teal" ];
-          tweakVariants = [
-            "outline"
-            "float"
-          ];
-        };
-        # name = "Catppuccin-Mocha-Standard-Teal-Dark";
-        # package = pkgs.catppuccin-gtk.override {
-        #   accents = ["teal"];
-        #   size = "standard";
-        #   tweaks = ["normal"];
-        #   variant = "mocha";
-        # };
+        package = materia-theme-transparent;
+        name = "Materia-dark-compact";
+        # name = "Tokyonight-Dark-BL";
+        # package = pkgs.tokyonight-gtk-theme.override {
+        #   themeVariants = [ "teal" ];
+        #   tweakVariants = [
+        #     "outline"
+        #     "float"
+        #   ];
       };
+      # name = "Catppuccin-Mocha-Standard-Teal-Dark";
+      # package = pkgs.catppuccin-gtk.override {
+      #   accents = ["teal"];
+      #   size = "standard";
+      #   tweaks = ["normal"];
+      #   variant = "mocha";
+      # };
+    };
 
-      iconTheme = {
-        # name = "Fluent-teal-dark";
-        # package = pkgs.fluent-icon-theme.override { colorVariants = [ "teal" ]; };
-        name = "Papirus-Dark";
-        # package = pkgs.papirus-icon-theme;
-        package = pkgs.catppuccin-papirus-folders.override {
-          flavor = "mocha";
-          accent = "teal";
-        };
-      };
-
-      cursorTheme = {
-        name = "Bibata-Modern-Ice";
-        package = pkgs.bibata-cursors;
-        size = 24;
-      };
-
-      gtk3.extraConfig = {
-        Settings = ''
-          gtk-application-prefer-dark-theme=1
-        '';
-      };
-
-      gtk4.extraConfig = {
-        Settings = ''
-          gtk-application-prefer-dark-theme=1
-        '';
+    iconTheme = {
+      # name = "Fluent-teal-dark";
+      # package = pkgs.fluent-icon-theme.override { colorVariants = [ "teal" ]; };
+      name = "Papirus-Dark";
+      # package = pkgs.papirus-icon-theme;
+      package = pkgs.catppuccin-papirus-folders.override {
+        flavor = "mocha";
+        accent = "teal";
       };
     };
 
-    qt = {
-      enable = true;
-      platformTheme.name = "gtk";
+    cursorTheme = {
+      name = "Bibata-Modern-Ice";
+      package = pkgs.bibata-cursors;
+      size = 24;
     };
 
-    dconf = {
-      enable = true;
-      settings = {
-        "org/gnome/desktop/interface" = {
-          color-scheme = "prefer-dark";
-        };
+    gtk3.extraConfig = {
+      Settings = ''
+        gtk-application-prefer-dark-theme=1
+      '';
+    };
+
+    gtk4.extraConfig = {
+      Settings = ''
+        gtk-application-prefer-dark-theme=1
+      '';
+    };
+  };
+
+  qt = {
+    enable = true;
+    platformTheme.name = "gtk";
+  };
+
+  dconf = {
+    enable = true;
+    settings = {
+      "org/gnome/desktop/interface" = {
+        color-scheme = "prefer-dark";
       };
     };
   };
