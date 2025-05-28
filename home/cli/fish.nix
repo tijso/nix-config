04899709e41cdfc1203bca1f3b642746bd7c910d
Joey Sonnier <tijso@protonmail.com>
2025-05-28 12:31:52 -0500

    changed fish theme

--- home/cli/fish.nix
+++ home/cli/fish.nix
@@ -141,27 +141,41 @@ in
         # set -g fish_pager_color_description 636da6
         # set -g fish_pager_color_selected_background --background=2d3f76 
 
-        # Nightfox
-        set -g fish_color_normal cdcecf
-        set -g fish_color_command 63cdcf
-        set -g fish_color_keyword d67ad2
-        set -g fish_color_quote dbc074
-        set -g fish_color_redirection cdcecf
-        set -g fish_color_end f4a261
-        set -g fish_color_error c94f6d
-        set -g fish_color_param 9d79d6
-        set -g fish_color_comment 738091
-        set -g fish_color_selection --background=2b3b51
-        set -g fish_color_search_match --background=2b3b51
-        set -g fish_color_operator 81b29a
-        set -g fish_color_escape d67ad2
-        set -g fish_color_autosuggestion 738091
+        # Nightfly
+        set -g fish_color_autosuggestion 4b6479
+        set -g fish_color_cancel 4b6479
+        set -g fish_color_command 7cb3ff
+        set -g fish_color_comment 7c8f8f --italics
+        set -g fish_color_cwd 87d787
+        set -g fish_color_cwd_root ff5874
+        set -g fish_color_end 7c8f8f
+        set -g fish_color_error fc514e
+        set -g fish_color_escape 7c8f8f
+        set -g fish_color_history_current c3ccdc --background=1d3b53
+        set -g fish_color_host d6deeb
+        set -g fish_color_host_remote d6deeb
+        set -g fish_color_keyword c792ea
+        set -g fish_color_match c3ccdc --background=1d3b53
+        set -g fish_color_normal bdc1c6
+        set -g fish_color_operator ff5874
+        set -g fish_color_option bdc1c6
+        set -g fish_color_param 61d5ae
+        set -g fish_color_quote ecc48d
+        set -g fish_color_redirection a1cd5e
+        set -g fish_color_search_match --background=1d3b53
+        set -g fish_color_selection --background=1d3b53
+        set -g fish_color_status fc514e
+        set -g fish_color_user 21c7a8
+        set -g fish_color_valid_path
 
-        # Completion Pager Colors
-        set -g fish_pager_color_progress 738091
-        set -g fish_pager_color_prefix 63cdcf
-        set -g fish_pager_color_completion cdcecf
-        set -g fish_pager_color_description 738091
+        # Completion pager colors.
+        set -g fish_pager_color_completion c3ccdc
+        set -g fish_pager_color_description 7c8f8f
+        set -g fish_pager_color_prefix 82aaff
+        set -g fish_pager_color_progress 7c8f8f
+        set -g fish_pager_color_selected_background --background=1d3b53
+        set -g fish_pager_color_selected_completion d6deeb
+        set -g fish_pager_color_selected_description d6deeb
 
         # Carbonfox
         # set -g fish_color_normal f2f4f8
