4e348dfbeb6c5e795f4bd9628180c8b8220cad72
Joey Sonnier <tijso@protonmail.com>
2025-06-09 05:04:49 -0500

    maybe

--- home/terminals/ghostty.nix
+++ home/terminals/ghostty.nix
@@ -12,11 +12,11 @@ in
   options.modules.terminals.ghostty.enable = mkEnableOption "Enable Ghostty";
   config = mkIf cfg.enable {
     programs.ghostty = {
-      enable = true;
+      # enable = true;
       enableBashIntegration = true;
       enableZshIntegration = true;
       enableFishIntegration = true;
-      # package = pkgs.ghostty;
+      package = pkgs.ghostty;
       settings = {
         font-family = "Maple Mono NF";
         font-family-bold = "Maple Mono NF Bold";
