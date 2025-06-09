a8406fed3ef4199a3c7cfb4e1608bc13e063c8b9
Joey Sonnier <tijso@protonmail.com>
2025-06-09 18:19:38 -0500

    updadte prompt

--- home/cli/starship.nix
+++ home/cli/starship.nix
@@ -52,8 +52,8 @@ in
         };
 
         character = {
-          success_symbol = "[ •   ](#31748f bold) ";
-          error_symbol = "[ •  󰅙 ](#eb6f92 bold) ";
+          success_symbol = "[ •   ](#31748f bold)";
+          error_symbol = "[ •  󰅙 ](#eb6f92 bold)";
           # success_symbol = "[ •   ](fg:#${config.colorScheme.palette.base0A}) ";
           # error_symbol = "[ •  󰅙 ](fg:#${config.colorScheme.palette.base08})[✘](fg:#${config.colorScheme.palette.base09})[├->](fg:#${config.colorScheme.palette.base0F}) ";
         };
