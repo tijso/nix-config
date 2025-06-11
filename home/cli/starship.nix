22897de3f8d3e26ebb13c0d688416055f6934478
Joey Sonnier <tijso@protonmail.com>
2025-06-11 11:12:09 -0500

    maybe

--- home/cli/starship.nix
+++ home/cli/starship.nix
@@ -127,7 +127,7 @@ in
 
         git_branch = {
           format = "[ $symbol $branch ]($style)";
-          style = "bg:overlay fg:foam";
+          style = "fg:foam";
           # style = "fg:#${config.colorScheme.palette.base0E}";
           symbol = " ";
         };
