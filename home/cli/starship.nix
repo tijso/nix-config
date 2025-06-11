bea27654e4122c21b21692a03d11ce3a6e6b771b
Joey Sonnier <tijso@protonmail.com>
2025-06-11 11:24:08 -0500

    changing theme

--- home/cli/starship.nix
+++ home/cli/starship.nix
@@ -127,7 +127,7 @@ in
 
         git_branch = {
           format = "[ $symbol $branch ]($style)";
-          style = "bg:overlay fg:foam";
+          style = "bg:base fg:foam";
           # style = "fg:#${config.colorScheme.palette.base0E}";
           symbol = " ";
         };
