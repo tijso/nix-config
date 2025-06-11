d012bbf219309d74a4d088d27f7f0e7c54c0438b
Joey Sonnier <tijso@protonmail.com>
2025-06-11 17:21:21 -0500

    updated

--- home/cli/starship.nix
+++ home/cli/starship.nix
@@ -128,7 +128,7 @@ in
         };
 
         git_branch = {
-          format = "[on](white) [$symbol$branch ]($style)";
+          format = "[on] [$symbol$branch ]($style)";
           style = "fg:foam";
           # style = "bg:overlay fg:foam";
           # style = "fg:#${config.colorScheme.palette.base0E}";
