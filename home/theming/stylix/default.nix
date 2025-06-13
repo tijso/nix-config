a99d345c4cf4e5d7da19d748d2349a9e19c5eb3a
Joey Sonnier <tijso@protonmail.com>
2025-06-13 00:54:50 -0500

    about to switch to stylix

--- home/theming/stylix/default.nix
+++ home/theming/stylix/default.nix
@@ -12,7 +12,6 @@ in
 {
   imports = with inputs; [
     stylix.homeManagerModules.stylix
-    # catppuccin.homeManagerModules.catppuccin
   ];
 
   options.theming.stylix = {
@@ -20,18 +19,42 @@ in
   };
 
   config = lib.mkIf cfg.enable {
-    home.packages = with pkgs; [
-      (nerdfonts.override { fonts = [ "NerdFontsSymbolsOnly" ]; })
-      google-fonts
-      plemoljp
-    ];
-
     stylix = {
       enable = true;
-      autoEnable = true;
-      base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
-
       image = pkgs.nixicle.wallpapers.earth;
+      polarity = "dark";
+
+      # base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
+      base16Scheme = {
+        system = "base16";
+        name = "Rosé Pine";
+        author = "Emilia Dunfelt <edun@dunfelt.se>";
+        slug = "rose-pine";
+        variant = "dark";
+        palette = {
+          base00 = "191724";
+          base01 = "1f1d2e";
+          base02 = "26233a";
+          base03 = "6e6a86";
+          base04 = "908caa";
+          base05 = "e0def4";
+          base06 = "e0def4";
+          base07 = "524f67";
+          base08 = "eb6f92";
+          base09 = "f6c177";
+          base0A = "ebbcba";
+          base0B = "31748f";
+          base0C = "9ccfd8";
+          base0D = "c4a7e7";
+          base0E = "f6c177";
+          base0F = "524f67";
+        };
+      };
+
+      opacity = {
+        terminal = opacity;
+        popups = opacity;
+      };
 
       cursor = {
         name = "Bibata-Modern-Classic";
@@ -39,31 +62,31 @@ in
         size = 24;
       };
 
+      serif = {
+        name = "Source Serif";
+        package = pkgs.source-serif;
+      };
+
+      sansSerif = {
+        name = "Noto Sans";
+        package = pkgs.noto-fonts;
+      };
+
+      monospace = {
+        package = pkgs.maple-mono.NF;
+        name = "Maple Mono NF";
+      };
+
+      emoji = {
+        package = pkgs.noto-fonts-emoji;
+        name = "Noto Color Emoji";
+      };
+
       fonts = {
         sizes = {
           terminal = 14;
           applications = 12;
           popups = 12;
-        };
-
-        serif = {
-          name = "Source Serif";
-          package = pkgs.source-serif;
-        };
-
-        sansSerif = {
-          name = "Noto Sans";
-          package = pkgs.noto-fonts;
-        };
-
-        monospace = {
-          package = pkgs.nixicle.monolisa;
-          name = "MonoLisa Nerd Font";
-        };
-
-        emoji = {
-          package = pkgs.noto-fonts-emoji;
-          name = "Noto Color Emoji";
         };
       };
     };
