051f84d2257b7de987ae33f851ed22cea8fbac89
Joey Sonnier <tijso@protonmail.com>
2025-06-15 00:26:05 -0500

    heres hoping

--- hosts/serenity/default.nix
+++ hosts/serenity/default.nix
@@ -7,11 +7,19 @@
   imports = [
     ./hardware-configuration.nix
 
+    ../../modules
     ../common
     ../users/global.nix
     ../../development
     ../../environments
   ];
+
+  myModules = {
+    thunar.enable = true;
+    # development.enable = false;
+    # gaming.enable = false;
+    # virtualization.enable = false;
+  };
 
   system.stateVersion = "23.11";
 }
