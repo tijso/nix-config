62d9ae065c6254de536777aa1f2a5254eeb5df76
Joey Sonnier <tijso@protonmail.com>
2025-06-16 17:21:29 -0500

    fixing

--- home/modules/desktop/niri/default.nix
+++ home/modules/desktop/niri/default.nix
@@ -11,13 +11,14 @@ with lib;
     inputs.niri.homeModules.niri
   ];
 
-  home.packages = with pkgs; [
-    niri
-    waybar
-    fuzzel
-  ];
   options.myModules.desktop.niri.enable = mkEnableOption "Enable Niri";
   config = mkIf config.myModules.desktop.niri.enable {
+    home.packages = with pkgs; [
+      niri
+      waybar
+      fuzzel
+    ];
+
     programs.niri = {
       enable = true;
       settings = {
