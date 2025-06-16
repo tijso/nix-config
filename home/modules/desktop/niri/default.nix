239ba5827bfccf89629e0601d2697139acf1de6f
Joey Sonnier <tijso@protonmail.com>
2025-06-16 16:20:39 -0500

    adding niri

---
+++ home/modules/desktop/niri/default.nix
@@ -0,0 +1,46 @@
+{
+  inputs,
+  pkgs,
+  config,
+  lib,
+  ...
+}:
+with lib;
+{
+  imports = [
+    inputs.niri.homeModules.niri
+  ];
+
+  options.myModules.desktop.niri.enable = mkEnableOption "Enable Niri";
+  config = mkIf config.myModules.desktop.niri.enable {
+    home.packages = with pkgs; [
+      niri
+      waybar
+      fuzzel
+    ];
+
+    programs.niri = {
+      enable = true;
+      settings = {
+        input = {
+          keyboard.xkb.layout = "us";
+        };
+
+        layout = {
+          gaps = 16;
+          center-focused-column = "never";
+        };
+
+        spawn-at-startup = [
+          { command = [ "${pkgs.waybar}/bin/waybar" ]; }
+        ];
+
+        binds = with inputs.niri.lib.niri.actions; {
+          "Mod+Return".action = spawn "ghostty";
+          "Mod+D".action = spawn "${pkgs.fuzzel}/bin/fuzzel";
+          "Mod+Q".action = close-window;
+        };
+      };
+    };
+  };
+}
