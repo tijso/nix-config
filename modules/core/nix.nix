a3d6d1faf973fa159132547e6a2a6f8934754f7a
Joey Sonnier <tijso@protonmail.com>
2025-06-16 19:52:07 -0500

    updated flake

--- modules/core/nix.nix
+++ modules/core/nix.nix
@@ -20,12 +20,14 @@ with lib;
           "https://prayag2.cachix.org/"
           "https://nix-community.cachix.org"
           "https://hyprland.cachix.org"
+          "https://niri.cachix.org/ https://cache.nixos.org/"
         ];
         trusted-public-keys = [
           "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
           "prayag2.cachix.org-1:Lu+JhgBkHI0vNG3VcZJAXTniydou+5bUiqf67kEcA4g="
           "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
           "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+          "niri.cachix.org-1:Wv0OmqXIU4e6X/mx2KViV9q/U/kLyRjX6XcbxfqAbM0= cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         ];
       };
       gc = {
