fe47e7b4482ef4a45ea35ad848206b4f88b0eb30
Joey Sonnier <tijso@protonmail.com>
2025-06-20 12:41:32 -0500

    removed commited lines i dont't need

--- hosts/serenity/home.nix
+++ hosts/serenity/home.nix
@@ -119,15 +119,6 @@
     enableFishIntegration = true;
   };
 
-  # programs.direnv = {
-  #   enable = true;
-  #   enableBashIntegration = true;
-  #   enableFishIntegration = true;
-  #   enableZshIntegration = true;
-  #   enableNushellIntegration = true;
-  #   nix-direnv.enable = true;
-  # };
-
   # xdg = {
   #   enable = true;
   #   userDirs = {
