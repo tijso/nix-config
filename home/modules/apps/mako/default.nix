984d32131be990d7d176264c9b50c3f9e9bbf7f3
Joey Sonnier <tijso@protonmail.com>
2025-06-21 15:51:37 -0500

    fixing mako

--- home/modules/apps/mako/default.nix
+++ home/modules/apps/mako/default.nix
@@ -10,18 +10,18 @@ with lib;
     services.mako = {
       enable = true;
       settings = {
-        backgroundColor = "${config.colorScheme.palette.base00}";
-        textColor = "${config.colorScheme.palette.base05}";
-        borderColor = "${config.colorScheme.palette.base0D}";
-        borderRadius = 12;
-        borderSize = 2;
+        background-color = "#${config.colorScheme.palette.base00}";
+        text-color = "#${config.colorScheme.palette.base05}";
+        border-color = "#${config.colorScheme.palette.base0D}";
+        border-radius = 12;
+        border-size = 2;
         font = "JetBrains Mono 14";
         padding = "12";
         margin = "12";
         width = 350;
         height = 150;
-        defaultTimeout = 5000;
-        groupBy = "summary";
+        default-timeout = 5000;
+        group-by = "summary";
       };
     };
   };
