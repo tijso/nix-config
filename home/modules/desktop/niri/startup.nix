ecb2e9b2c9e2b242506553e140ec7029f82014d8
Joey Sonnier <tijso@protonmail.com>
2025-06-21 13:16:23 -0500

    fixing

--- home/modules/desktop/niri/startup.nix
+++ home/modules/desktop/niri/startup.nix
@@ -19,15 +19,22 @@
   systemd.user.services.set-wallpaper = {
     Unit = {
       Description = "Set desktop wallpaper";
-      After = [ "swww.service" ];
-      Wants = [ "swww.service" ];
+      # Use the correct service name: swww.service
+      After = [
+        "swww.service"
+        "graphical-session.target"
+      ];
+      Requires = [ "swww.service" ];
+      Wants = [ "graphical-session.target" ];
     };
-
     Service = {
       Type = "oneshot";
+      # Add a small delay to ensure swww daemon is fully ready
+      ExecStartPre = "${pkgs.coreutils}/bin/sleep 1";
       ExecStart = "${pkgs.swww}/bin/swww img ${../../theming/Wallpapers/serenity.jpg} --transition-type wipe --transition-duration 2";
+      # Add restart behavior
+      RemainAfterExit = true;
     };
-
     Install = {
       WantedBy = [ "graphical-session.target" ];
     };
