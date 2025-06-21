b86ba34ee2c88645bd888dfed9f737317af908d1
Joey Sonnier <tijso@protonmail.com>
2025-06-21 05:38:06 -0500

    fixing

--- home/modules/desktop/niri/startup.nix
+++ home/modules/desktop/niri/startup.nix
@@ -3,20 +3,33 @@
   ...
 }:
 {
+  # Regular startup programs
   programs.niri.settings.spawn-at-startup = [
     { command = [ "${pkgs.waybar}/bin/waybar" ]; }
-    {
-      command = [
-        "init"
-        "${pkgs.swww}/bin/swww"
-        "img"
-        "${../../theming/Wallpapers/serenity.jpg}"
-        "--transition-type"
-        "wipe"
-        "--transition-duration"
-        "2"
-      ];
-    }
     { command = [ "${pkgs.mako}/bin/mako" ]; }
   ];
+
+  # SWWW service and wallpaper setup
+  services.swww = {
+    enable = true;
+    package = pkgs.swww;
+  };
+
+  # Set wallpaper after swww starts
+  systemd.user.services.set-wallpaper = {
+    Unit = {
+      Description = "Set desktop wallpaper";
+      After = [ "swww.service" ];
+      Wants = [ "swww.service" ];
+    };
+
+    Service = {
+      Type = "oneshot";
+      ExecStart = "${pkgs.swww}/bin/swww img ${../../theming/Wallpapers/serenity.jpg} --transition-type wipe --transition-duration 2";
+    };
+
+    Install = {
+      WantedBy = [ "graphical-session.target" ];
+    };
+  };
 }
