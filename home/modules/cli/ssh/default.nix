572b36d6eedac38049b2c33278c1d351c7f1bd1d
Joey Sonnier <tijso@protonmail.com>
2025-06-21 07:35:02 -0500

    maybe

--- home/modules/cli/ssh/default.nix
+++ home/modules/cli/ssh/default.nix
@@ -1,35 +1,32 @@
 {
-  pkgs,
   lib,
   config,
   ...
 }:
 with lib;
-
 {
   options.myHome.cli.ssh.enable = mkEnableOption "Enable Ssh";
   config = mkIf config.myHome.cli.ssh.enable {
-
     programs.ssh = {
       enable = true;
       addKeysToAgent = "yes";
+      startAgent = true;
+      matchBlocks = {
+        "github.com" = {
+          hostname = "github.com";
+          user = "git";
+          identityFile = [ "~/.ssh/github" ]; # <--- Adjust this path
+          identitiesOnly = true;
+        };
+      };
+
+      # Optional: Any other SSH client config you want to add globally
+      # extraConfig = ''
+      #   Host *
+      #     ServerAliveInterval 60
+      # '';
     };
 
-    # Add this service to ensure SSH agent starts with your session
-    systemd.user.services.ssh-agent = {
-      Unit = {
-        Description = "SSH key agent";
-        Before = [ "graphical-session-pre.target" ];
-        Wants = [ "graphical-session-pre.target" ];
-      };
-      Service = {
-        Type = "forking";
-        Environment = "SSH_AUTH_SOCK=%t/ssh-agent.socket";
-        ExecStart = "${pkgs.openssh}/bin/ssh-agent -a $SSH_AUTH_SOCK";
-      };
-      Install = {
-        WantedBy = [ "default.target" ];
-      };
-    };
+    users.users.${config.home.username}.extraGroups = [ "ssh-agent" ];
   };
 }
