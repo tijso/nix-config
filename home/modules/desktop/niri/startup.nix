d07ddc7a1f59dd2807cd3260303e44afe7dd0e8c
Joey Sonnier <tijso@protonmail.com>
2025-06-20 21:16:54 -0500

    fixing

--- home/modules/desktop/niri/startup.nix
+++ home/modules/desktop/niri/startup.nix
@@ -7,8 +7,15 @@
     { command = [ "${pkgs.waybar}/bin/waybar" ]; }
     {
       command = [
-        "${pkgs.swww}/bin/swww"
         "init"
+        "${pkgs.swww}/bin/swww"
+        "img"
+        "${../../theming/Wallpapers/serenity.jpg}"
+        "--transition-type"
+        "wipe"
+        "--transition-duration"
+        "2"
+        # "${pkgs.swww}/bin/swww """
       ];
     }
     { command = [ "${pkgs.mako}/bin/mako" ]; }
