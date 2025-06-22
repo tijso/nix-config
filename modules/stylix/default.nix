23141c6fd348ec908f71dce201ecc00fc9cb2fd1
Joey Sonnier <tijso@protonmail.com>
2025-06-22 01:02:01 -0500

    updated stylix

--- modules/stylix/default.nix
+++ modules/stylix/default.nix
@@ -17,33 +17,7 @@ with lib;
       enable = true;
       polarity = "dark";
       image = ./Wallpapers/castle-landscape.jpg;
-
       base16Scheme = "./themes/rose-pine.yaml";
-      # base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
-      # base16Scheme = {
-      #   system = "base16";
-      #   name = "Rosé Pine";
-      #   author = "Emilia Dunfelt <edun@dunfelt.se>";
-      #   slug = "rose-pine";
-      #   variant = "dark";
-      #   palette = {
-      #     base00 = "191724";
-      #     base01 = "1f1d2e";
-      #     base02 = "26233a";
-      #     base03 = "6e6a86";
-      #     base04 = "908caa";
-      #     base05 = "e0def4";
-      #     base06 = "e0def4";
-      #     base07 = "524f67";
-      #     base08 = "eb6f92";
-      #     base09 = "f6c177";
-      #     base0A = "ebbcba";
-      #     base0B = "31748f";
-      #     base0C = "9ccfd8";
-      #     base0D = "c4a7e7";
-      #     base0E = "f6c177";
-      #     base0F = "524f67";
-      #   };
     };
 
     opacity = {
