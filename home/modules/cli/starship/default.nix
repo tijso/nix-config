4be7fe4725ad682a347d044f73b596ecb9d1fb25
Joey Sonnier <tijso@protonmail.com>
2025-06-22 14:27:42 -0500

    still working

--- home/modules/cli/starship/default.nix
+++ home/modules/cli/starship/default.nix
@@ -37,14 +37,14 @@ with lib;
         ];
 
         character = {
-          success_symbol = "[ •   ](fg:#${config.colorScheme.palette.base0B})";
-          error_symbol = "[ •  󰅙 ](fg:#${config.colorScheme.palette.base08})";
+          success_symbol = "[ •   ](fg:#${config.lib.stylix.colors.base0B})";
+          error_symbol = "[ •  󰅙 ](fg:#${config.lib.stylix.colors.base08})";
         };
 
         directory = {
           format = "[ $path ]($style)";
           fish_style_pwd_dir_length = 1;
-          style = "fg:#${config.colorScheme.palette.base0B}";
+          style = "fg:#${config.lib.stylix.colors.base0B}";
           truncation_length = 3;
           truncation_symbol = "…/";
         };
@@ -62,12 +62,12 @@ with lib;
 
         cmd_duration = {
           format = "[ $duration 󱑂 ]($style)";
-          style = "fg:#${config.colorScheme.palette.base0A}";
+          style = "fg:#${config.lib.stylix.colors.base0A}";
         };
 
         jobs = {
           symbol = " ";
-          style = "fg:#${config.colorScheme.palette.base08}";
+          style = "fg:#${config.lib.stylix.colors.base08}";
           number_threshold = 1;
           format = "[$symbol]($style)";
         };
@@ -76,13 +76,13 @@ with lib;
           disabled = false;
           ssh_only = false;
           format = "[ $ssh_symbol$hostname ]($style)";
-          style = "fg:#${config.colorScheme.palette.base0A}";
+          style = "fg:#${config.lib.stylix.colors.base0A}";
         };
 
         os = {
           disabled = false;
           format = "[ $symbol ]($style)";
-          style = "fg:#${config.colorScheme.palette.base0A}";
+          style = "fg:#${config.lib.stylix.colors.base0A}";
           symbols = {
             NixOS = "[ ]($style)";
             # Arch = "[ ]($style)";
@@ -91,99 +91,99 @@ with lib;
 
         git_branch = {
           symbol = " ";
-          format = "[on](fg:#${config.colorScheme.palette.base04}) [$symbol$branch ]($style)";
-          style = "fg:#${config.colorScheme.palette.base0C}";
+          format = "[on](fg:#${config.lib.stylix.colors.base04}) [$symbol$branch ]($style)";
+          style = "fg:#${config.lib.stylix.colors.base0C}";
         };
 
         git_status = {
           disabled = false;
-          style = "fg:#${config.colorScheme.palette.base0C}";
+          style = "fg:#${config.lib.stylix.colors.base0C}";
           format = "([$all_status$ahead_behind]($style))";
-          up_to_date = "[ ✓ ](fg:#${config.colorScheme.palette.base0C})";
-          untracked = "[?($count)](fg:#${config.colorScheme.palette.base09})";
-          stashed = "[$](fg:#${config.colorScheme.palette.base0D})";
-          modified = "[!($count)](fg:#${config.colorScheme.palette.base09})";
-          renamed = "[»($count)](fg:#${config.colorScheme.palette.base0D})";
-          deleted = "[✘($count)](fg:#${config.colorScheme.palette.base08})";
-          staged = "[++($count)](fg:#${config.colorScheme.palette.base0C})";
-          ahead = "[⇡($count)](fg:#${config.colorScheme.palette.base0C})";
-          diverged = "⇕[[](fg:iris)[⇡($ahead_count)](fg:foam)[⇣($behind_count)](fg:#${config.colorScheme.palette.base08})[]]";
-          behind = "[⇣($count)](fg:#${config.colorScheme.palette.base08})";
+          up_to_date = "[ ✓ ](fg:#${config.lib.stylix.colors.base0C})";
+          untracked = "[?($count)](fg:#${config.lib.stylix.colors.base09})";
+          stashed = "[$](fg:#${config.lib.stylix.colors.base0D})";
+          modified = "[!($count)](fg:#${config.lib.stylix.colors.base09})";
+          renamed = "[»($count)](fg:#${config.lib.stylix.colors.base0D})";
+          deleted = "[✘($count)](fg:#${config.lib.stylix.colors.base08})";
+          staged = "[++($count)](fg:#${config.lib.stylix.colors.base0C})";
+          ahead = "[⇡($count)](fg:#${config.lib.stylix.colors.base0C})";
+          diverged = "⇕[[](fg:iris)[⇡($ahead_count)](fg:foam)[⇣($behind_count)](fg:#${config.lib.stylix.colors.base08})[]]";
+          behind = "[⇣($count)](fg:#${config.lib.stylix.colors.base08})";
         };
 
         golang = {
           symbol = " ";
-          style = "fg:#${config.colorScheme.palette.base0B}";
+          style = "fg:#${config.lib.stylix.colors.base0B}";
           format = "[ $symbol$version ]($style)";
           disabled = false;
         };
 
         lua = {
           symbol = "  ";
-          style = "fg:#${config.colorScheme.palette.base0B}";
+          style = "fg:#${config.lib.stylix.colors.base0B}";
           format = "[ $symbol$version ]($style)";
           disabled = false;
         };
 
         c = {
           symbol = " ";
-          style = "fg:#${config.colorScheme.palette.base0B}";
+          style = "fg:#${config.lib.stylix.colors.base0B}";
           format = "[ $symbol$version ]($style)";
           disabled = false;
         };
 
         rust = {
           symbol = " ";
-          style = "fg:#${config.colorScheme.palette.base08}";
+          style = "fg:#${config.lib.stylix.colors.base08}";
           format = "[ $symbol$version ]($style)";
           disabled = false;
         };
 
         python = {
           symbol = " ";
-          style = "fg:#${config.colorScheme.palette.base09}";
+          style = "fg:#${config.lib.stylix.colors.base09}";
           format = "[ $symbol$version ]($style)";
           disabled = false;
         };
 
         nix_shell = {
           symbol = " ";
-          style = "fg:#${config.colorScheme.palette.base0B}";
+          style = "fg:#${config.lib.stylix.colors.base0B}";
           format = "[ $symbol$version ]($style)";
           disabled = false;
         };
 
         haskell = {
           symbol = " ";
-          style = "fg:#${config.colorScheme.palette.base0D}";
+          style = "fg:#${config.lib.stylix.colors.base0D}";
           format = "[ $symbol$version ]($style)";
           disabled = false;
         };
 
         java = {
           symbol = " ";
-          style = "fg:#${config.colorScheme.palette.base0A}";
+          style = "fg:#${config.lib.stylix.colors.base0A}";
           format = "[ $symbol$version ]($style)";
           disabled = false;
         };
 
         scala = {
           symbol = " ";
-          style = "fg:#${config.colorScheme.palette.base0A}";
+          style = "fg:#${config.lib.stylix.colors.base0A}";
           format = "[ $symbol$version ]($style)";
           disabled = false;
         };
 
         nodejs = {
           symbol = "󰎙 ";
-          style = "fg:#${config.colorScheme.palette.base0A}";
+          style = "fg:#${config.lib.stylix.colors.base0A}";
           format = "[ $symbol$version ]($style)";
           disabled = false;
         };
 
         nim = {
           symbol = "󰆥 ";
-          style = "fg:#${config.colorScheme.palette.base0E}";
+          style = "fg:#${config.lib.stylix.colors.base0E}";
           format = "[ $symbol$version ]($style)";
           disabled = false;
         };
