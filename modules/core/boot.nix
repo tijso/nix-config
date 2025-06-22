3625fc99ffbccfd68c9ca0b293ad0ec2dc7443d0
Joey Sonnier <tijso@protonmail.com>
2025-06-22 17:55:27 -0500

    more fix

--- modules/core/boot.nix
+++ modules/core/boot.nix
@@ -8,58 +8,52 @@ with lib;
 {
   options.myModules.core.boot = {
     enable = mkEnableOption "Enable boot configuration";
-    config = mkIf config.myModules.core.boot.enable {
-      boot = {
-        # Kernel configuration
-        kernelPackages = pkgs.linuxPackages_6_6;
-        # kernelPackages = pkgs.linuxPackages_latest;
-        kernelModules = [ "v4l2loopback" ];
-        kernelParams = [
-          "quiet"
-          "splash"
-          "plymouth.enable=1"
-        ];
-        consoleLogLevel = 0;
+    plymouth = {
+      enable = mkEnableOption "Enable Plymouth boot splash";
+    };
+  };
 
-        # System control parameters
-        kernel.sysctl = {
-          "vm.max_map_count" = 2147483642;
-        };
-
-        # Initial ramdisk configuration
-        initrd = {
-          verbose = false;
-          systemd.enable = true;
-        };
-
-        # V4L2 loopback module
-        extraModulePackages = [
-          (config.boot.kernelPackages.v4l2loopback.overrideAttrs (old: {
-            version = "0.15.0";
-            src = pkgs.fetchFromGitHub {
-              owner = "umlaeute";
-              repo = "v4l2loopback";
-              rev = "v0.15.0";
-              sha256 = "sha256-fa3f8GDoQTkPppAysrkA7kHuU5z2P2pqI8dKhuKYh04=";
-            };
-          }))
-        ];
-
-        plymouth = mkIf config.myModules.core.boot.plymouth.enable true;
-
-        loader = {
-          efi = {
-            canTouchEfiVariables = true;
-            efiSysMountPoint = "/boot";
+  config = mkIf config.myModules.core.boot.enable {
+    boot = {
+      kernelPackages = pkgs.linuxPackages_6_6;
+      # kernelPackages = pkgs.linuxPackages_latest;
+      kernelModules = [ "v4l2loopback" ];
+      kernelParams = [
+        "quiet"
+        "splash"
+        "plymouth.enable=1"
+      ];
+      consoleLogLevel = 0;
+      kernel.sysctl = {
+        "vm.max_map_count" = 2147483642;
+      };
+      initrd = {
+        verbose = false;
+        systemd.enable = true;
+      };
+      extraModulePackages = [
+        (config.boot.kernelPackages.v4l2loopback.overrideAttrs (old: {
+          version = "0.15.0";
+          src = pkgs.fetchFromGitHub {
+            owner = "umlaeute";
+            repo = "v4l2loopback";
+            rev = "v0.15.0";
+            sha256 = "sha256-fa3f8GDoQTkPppAysrkA7kHuU5z2P2pqI8dKhuKYh04=";
           };
-
-          grub = {
-            enable = true;
-            devices = [ "nodev" ];
-            efiSupport = true;
-            useOSProber = true;
-            configurationLimit = 10;
-          };
+        }))
+      ];
+      plymouth.enable = mkIf config.myModules.core.boot.plymouth.enable true;
+      loader = {
+        efi = {
+          canTouchEfiVariables = true;
+          efiSysMountPoint = "/boot";
+        };
+        grub = {
+          enable = true;
+          devices = [ "nodev" ];
+          efiSupport = true;
+          useOSProber = true;
+          configurationLimit = 10;
         };
       };
     };
