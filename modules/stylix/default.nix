fe287ad18c9fe005550e7cdebb3585456ff8145c
Joey Sonnier <tijso@protonmail.com>
2025-06-22 13:27:27 -0500

    cleaning up

--- modules/stylix/default.nix
+++ modules/stylix/default.nix
@@ -11,37 +11,32 @@ with lib;
   config = mkIf config.myModules.stylix.enable {
     stylix = {
       enable = true;
+      autoEnable = false;
       polarity = "dark";
 
       # System wallpaper and colors
-      image = ./assets/wallpapers/system-wallpaper.png;
-      base16Scheme = "${pkgs.base16-schemes}/share/themes/rose-pine.yaml";
+      image = ./Berserk.png;
+      base16Scheme = "./themes/rose-pine.yaml";
 
       targets = {
         grub.enable = true;
         plymouth.enable = true;
-        lightdm.enable = true;
+        greetd.enable = true;
         console.enable = true;
 
-        # Home-Manager takes take of the rest
-        gtk.enable = false;
-        qt.enable = false;
-        firefox.enable = false;
-        fuzzel.enable = false;
-        waybar.enable = false;
-      };
-
-      # Basic system fonts
-      fonts = {
-        monospace = {
-          name = "FiraCode Nerd Font";
-          package = pkgs.nerdfonts.override { fonts = [ "FiraCode" ]; };
-        };
-        sansSerif = {
-          name = "Noto Sans";
-          package = pkgs.noto-fonts;
+        # Basic system fonts
+        fonts = {
+          monospace = {
+            name = "FiraCode Nerd Font";
+            package = pkgs.nerdfonts.override { fonts = [ "FiraCode" ]; };
+          };
+          sansSerif = {
+            name = "Noto Sans";
+            package = pkgs.noto-fonts;
+          };
         };
       };
     };
+
   };
 }
