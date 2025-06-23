c133e24d600f52fc5c09cc347acdb399ae806f25
Joey Sonnier <tijso@protonmail.com>
2025-06-22 19:42:36 -0500

    still fixig

--- home/modules/cli/bat/default.nix
+++ home/modules/cli/bat/default.nix
@@ -11,20 +11,6 @@ with lib;
   config = mkIf config.myHome.cli.bat.enable {
     programs.bat = {
       enable = true;
-      config = {
-        theme = "tokyo-night";
-      };
-      themes = {
-        tokyo-night = {
-          src = pkgs.fetchFromGitHub {
-            owner = "folke";
-            repo = "tokyonight.nvim";
-            rev = "45d22cf0e1b93476d3b6d362d720412b3d34465c"; # Replace with latest commit
-            sha256 = "sha256-TJ/a6N6Cc1T0wdMxMopma1NtwL7rMYbZ6F0zFI1zaIA=";
-          };
-          file = "extras/sublime/tokyonight_night.tmTheme";
-        };
-      };
       extraPackages = with pkgs.bat-extras; [
         batdiff
         batgrep
