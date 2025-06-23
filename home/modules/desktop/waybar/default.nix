34257ad0e28d5ddcd431cb40525eacf6478b3bc2
Joey Sonnier <tijso@protonmail.com>
2025-06-23 17:20:42 -0500

    fixing

--- home/modules/desktop/waybar/default.nix
+++ home/modules/desktop/waybar/default.nix
@@ -39,7 +39,7 @@ in
           ];
 
           "custom/launcher" = {
-            format = " ";
+            format = " ";
             on-click = "${pkgs.fuzzel}/bin/fuzzel";
             tooltip = false;
           };
