773226ed242e224eff23585b2c76d7a4fa931f60
Joey Sonnier <tijso@protonmail.com>
2025-07-02 14:49:51 -0500

    changing kernel

--- modules/core/boot.nix
+++ modules/core/boot.nix
@@ -14,7 +14,16 @@ with lib; {
 
   config = mkIf config.myModules.core.boot.enable {
     boot = {
-      kernelPackages = pkgs.linuxPackages_6_6;
+      kernelPackages = pkgs.linuxPackagesFor (pkgs.linux_6_14.override {
+        argsOverride = rec {
+          src = pkgs.fetchurl {
+            url = "mirror://kernel/linux/kernel/v6.x/linux-${version}.tar.xz";
+            sha256 = "sha256-IYF/GZjiIw+B9+T2Bfpv3LBA4U+ifZnCfdsWznSXl6k=";
+          };
+          version = "6.14.6";
+          modDirVersion = "6.14.6";
+        };
+      });
       # kernelPackages = pkgs.linuxPackages_latest;
       kernelModules = ["v4l2loopback"];
       kernelParams = [
@@ -30,17 +39,17 @@ with lib; {
         verbose = false;
         systemd.enable = true;
       };
-      extraModulePackages = [
-        (config.boot.kernelPackages.v4l2loopback.overrideAttrs (old: {
-          version = "0.15.0";
-          src = pkgs.fetchFromGitHub {
-            owner = "umlaeute";
-            repo = "v4l2loopback";
-            rev = "v0.15.0";
-            sha256 = "sha256-fa3f8GDoQTkPppAysrkA7kHuU5z2P2pqI8dKhuKYh04=";
-          };
-        }))
-      ];
+      # extraModulePackages = [
+      #   (config.boot.kernelPackages.v4l2loopback.overrideAttrs (old: {
+      #     version = "0.15.0";
+      #     src = pkgs.fetchFromGitHub {
+      #       owner = "umlaeute";
+      #       repo = "v4l2loopback";
+      #       rev = "v0.15.0";
+      #       sha256 = "sha256-fa3f8GDoQTkPppAysrkA7kHuU5z2P2pqI8dKhuKYh04=";
+      #     };
+      #   }))
+      # ];
       plymouth.enable = mkIf config.myModules.core.boot.plymouth.enable true;
       loader = {
         efi = {
