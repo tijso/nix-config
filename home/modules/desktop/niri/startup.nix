544cac4d033efeb19a4e9d2a42a9ac73442b3130
Joey Sonnier <tijso@protonmail.com>
2025-07-02 15:12:56 -0500

    removing swww

--- home/modules/desktop/niri/startup.nix
+++ home/modules/desktop/niri/startup.nix
@@ -1,12 +1,8 @@
-{
-  pkgs,
-  ...
-}:
-{
+{pkgs, ...}: {
   # Regular startup programs
   programs.niri.settings.spawn-at-startup = [
-    { command = [ "${pkgs.waybar}/bin/waybar" ]; }
-    { command = [ "${pkgs.mako}/bin/mako" ]; }
+    {command = ["${pkgs.waybar}/bin/waybar"];}
+    {command = ["${pkgs.mako}/bin/mako"];}
     {
       command = [
         "${pkgs._1password-gui}/bin/1password"
@@ -16,33 +12,33 @@
   ];
 
   # SWWW service and wallpaper setup
-  services.swww = {
-    enable = true;
-    package = pkgs.swww;
-  };
+  # services.swww = {
+  #   enable = true;
+  #   package = pkgs.swww;
+  # };
 
   # Set wallpaper after swww starts
-  systemd.user.services.set-wallpaper = {
-    Unit = {
-      Description = "Set desktop wallpaper";
-      # Use the correct service name: swww.service
-      After = [
-        "swww.service"
-        "graphical-session.target"
-      ];
-      Requires = [ "swww.service" ];
-      Wants = [ "graphical-session.target" ];
-    };
-    Service = {
-      Type = "oneshot";
-      # Add a small delay to ensure swww daemon is fully ready
-      ExecStartPre = "${pkgs.coreutils}/bin/sleep 1";
-      ExecStart = "${pkgs.swww}/bin/swww img ${./serenity.jpg} --transition-type wipe --transition-duration 2";
-      # Add restart behavior
-      RemainAfterExit = true;
-    };
-    Install = {
-      WantedBy = [ "graphical-session.target" ];
-    };
-  };
+  # systemd.user.services.set-wallpaper = {
+  #   Unit = {
+  #     Description = "Set desktop wallpaper";
+  # Use the correct service name: swww.service
+  #   After = [
+  #     "swww.service"
+  #     "graphical-session.target"
+  #   ];
+  #   Requires = [ "swww.service" ];
+  #   Wants = [ "graphical-session.target" ];
+  # };
+  # Service = {
+  #   Type = "oneshot";
+  #   # Add a small delay to ensure swww daemon is fully ready
+  #   ExecStartPre = "${pkgs.coreutils}/bin/sleep 1";
+  #   ExecStart = "${pkgs.swww}/bin/swww img ${./serenity.jpg} --transition-type wipe --transition-duration 2";
+  #   # Add restart behavior
+  #   RemainAfterExit = true;
+  # };
+  # Install = {
+  #   WantedBy = [ "graphical-session.target" ];
+  # };
+  # };
 }
