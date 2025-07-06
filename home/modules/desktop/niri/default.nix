9e831022a88b85a88843c5f9469f7fca67782fdc
Joey Sonnier <tijso@protonmail.com>
2025-07-05 21:37:19 -0500

    fix-2

--- home/modules/desktop/niri/default.nix
+++ home/modules/desktop/niri/default.nix
@@ -58,6 +58,7 @@ with lib; {
           MOZ_ENABLE_WAYLAND = "1";
           NIXOS_OZONE_WAYLAND_AUTO = "1";
           XDG_CURRENT_DESKTOP = "Niri";
+          DISPLAY = "0";
         };
 
         input = {
