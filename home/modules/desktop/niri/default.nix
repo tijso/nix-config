923263e18d90becdbf4cb8ec74dd67b38be5bff9
Joey Sonnier <tijso@protonmail.com>
2025-07-05 21:31:44 -0500

    updated

--- home/modules/desktop/niri/default.nix
+++ home/modules/desktop/niri/default.nix
@@ -58,7 +58,6 @@ with lib; {
           MOZ_ENABLE_WAYLAND = "1";
           NIXOS_OZONE_WAYLAND_AUTO = "1";
           XDG_CURRENT_DESKTOP = "Niri";
-          DISPLAY = ":0";
         };
 
         input = {
