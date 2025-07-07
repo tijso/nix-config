36f1f317f02c503b05cadac5aade197979ce3c0b
Joey Sonnier <tijso@protonmail.com>
2025-07-07 15:08:24 -0500

    switching kernels

--- modules/core/boot.nix
+++ modules/core/boot.nix
@@ -14,16 +14,7 @@ with lib; {
 
   config = mkIf config.myModules.core.boot.enable {
     boot = {
-      kernelPackages = pkgs.linuxPackagesFor (pkgs.linux_6_14.override {
-        argsOverride = rec {
-          src = pkgs.fetchurl {
-            url = "mirror://kernel/linux/kernel/v6.x/linux-${version}.tar.xz";
-            sha256 = "sha256-IYF/GZjiIw+B9+T2Bfpv3LBA4U+ifZnCfdsWznSXl6k=";
-          };
-          version = "6.14.6";
-          modDirVersion = "6.14.6";
-        };
-      });
+      kernelPackages = pkgs.linuxPackages_6_12;
       # kernelPackages = pkgs.linuxPackages_latest;
       kernelModules = ["v4l2loopback"];
       kernelParams = [
@@ -39,17 +30,6 @@ with lib; {
         verbose = false;
         systemd.enable = true;
       };
-      # extraModulePackages = [
-      #   (config.boot.kernelPackages.v4l2loopback.overrideAttrs (old: {
-      #     version = "0.15.0";
-      #     src = pkgs.fetchFromGitHub {
-      #       owner = "umlaeute";
-      #       repo = "v4l2loopback";
-      #       rev = "v0.15.0";
-      #       sha256 = "sha256-fa3f8GDoQTkPppAysrkA7kHuU5z2P2pqI8dKhuKYh04=";
-      #     };
-      #   }))
-      # ];
       plymouth.enable = mkIf config.myModules.core.boot.plymouth.enable true;
       loader = {
         efi = {
