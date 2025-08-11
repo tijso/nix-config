c3667b840e4f34d4e121bbf8f6830556f1f3683b
Joey Sonnier <tijso@protonmail.com>
2025-08-11 10:49:22 -0500

    changed theme

--- home/modules/terminals/wezterm/default.nix
+++ home/modules/terminals/wezterm/default.nix
@@ -12,22 +12,20 @@ with lib; {
         return {
           -- color_scheme = "Catppuccin Mocha",
           -- color_scheme = "Tokyo Night",
-          -- color_scheme = "rose-pine",
-          -- color_scheme = "Kanagawa (Gogh)",
-          -- font = wezterm.font "Fira Code",
+          color_scheme = "rose-pine",
 
           -- my coolnight colorscheme:
-          colors = {
-         foreground = "#CBE0F0",
-         background = "#011423",
-         cursor_bg = "#47FF9C",
-         cursor_border = "#47FF9C",
-         cursor_fg = "#011423",
-         selection_bg = "#033259",
-         selection_fg = "#CBE0F0",
-         ansi = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#0FC5ED", "#a277ff", "#24EAF7", "#24EAF7" },
-         brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
-          },
+         -- colors = {
+         -- foreground = "#CBE0F0",
+         --background = "#011423",
+         --cursor_bg = "#47FF9C",
+         --cursor_border = "#47FF9C",
+         --cursor_fg = "#011423",
+         --selection_bg = "#033259",
+         --selection_fg = "#CBE0F0",
+         --ansi = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#0FC5ED", "#a277ff", "#24EAF7", "#24EAF7" },
+         --brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
+          --},
 
           font = wezterm.font "Maple Mono NF",
           -- font = wezterm.font "Monaspace Radon",
