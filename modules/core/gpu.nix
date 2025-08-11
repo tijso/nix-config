ab3cd472cf353240c57142505c1bdada97be89b6
Joey Sonnier <tijso@protonmail.com>
2025-08-11 10:25:51 -0500

    updated

--- modules/core/gpu.nix
+++ modules/core/gpu.nix
@@ -8,18 +8,21 @@ with lib; {
   options.myModules.core.gpu.enable = mkEnableOption "Enable Gpu";
 
   config = mkIf config.myModules.core.gpu.enable {
-    hardware = {
-      opengl.enable = true;
-      graphics = {
-        enable = true;
-        extraPackages = with pkgs; [
-          vulkan-loader
-          vulkan-validation-layers
-          vulkan-extension-layer
-          # rocmPackages.clr.icd
-          # amdvlk
-        ];
-      };
+    hardware.graphics = {
+      enable = true;
+      enable32Bit = true;
+      extraPackages = with pkgs; [
+        vulkan-loader
+        vulkan-validation-layers
+        vulkan-extension-layer
+        # rocmPackages.clr.icd
+        # amdvlk
+      ];
+    };
+    hardware.opengl = {
+      enable = true;
+      driSupport = true;
+      driSupport32Bit = true;
     };
 
     services.xserver.videoDrivers = ["amdgpu"];
