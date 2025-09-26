ee378a571a3f2345a8e1b80f4abc526836991701
Joey Sonnier <tijso@protonmail.com>
2025-09-26 12:00:54 -0500

    still fixing font size

--- modules/core/fonts/default.nix
+++ modules/core/fonts/default.nix
@@ -8,7 +8,21 @@ with lib; {
   options.myModules.core.fonts.enable = mkEnableOption "Enable Fonts To Install";
   config = mkIf config.myModules.core.fonts.enable {
     fonts = {
+      fontconfig = {
+        enable = true;
+        antialias = true;
+        hinting = {
+          enable = true;
+          style = "full";
+        };
+        subpixel = {
+          rgba = "rgb";
+        };
+      };
+
       fontDir.enable = true;
+      enableDefaultPackages = true;
+
       packages = with pkgs; [
         jetbrains-mono
         noto-fonts
