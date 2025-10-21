88752c0e6b6935c4805ac0f18ae3cdd04f30e62d
Joey Sonnier <tijso@protonmail.com>
2025-10-21 12:57:28 -0500

    fixing git

--- home/modules/development/git/default.nix
+++ home/modules/development/git/default.nix
@@ -4,8 +4,7 @@
   pkgs,
   ...
 }:
-with lib;
-{
+with lib; {
   options.myHome.development.git.enable = mkEnableOption "Enable Git";
   config = mkIf config.myHome.development.git.enable {
     home.packages = with pkgs; [
@@ -28,19 +27,21 @@ with lib;
     programs.git = {
       enable = true;
       package = pkgs.gitFull;
-      userName = "Joey Sonnier";
-      userEmail = "tijso@protonmail.com";
-      aliases = {
-        a = "!git add . && git status";
-        lg = "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr)%C(bold blue)<%an>%Creset' --abbrev-commit";
-        pu = "push";
-        co = "checkout";
-        cm = "commit";
-      };
-      extraConfig = {
-        init.defaultBranch = "main";
-        push.autoSetupRemote = true;
-        pull.rebase = true;
+      settings = {
+        userName = "Joey Sonnier";
+        userEmail = "tijso@protonmail.com";
+        aliases = {
+          a = "!git add . && git status";
+          lg = "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr)%C(bold blue)<%an>%Creset' --abbrev-commit";
+          pu = "push";
+          co = "checkout";
+          cm = "commit";
+        };
+        extraConfig = {
+          init.defaultBranch = "main";
+          push.autoSetupRemote = true;
+          pull.rebase = true;
+        };
       };
     };
   };
